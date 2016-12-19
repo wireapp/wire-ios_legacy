aedd217d5d41260ac18a5dfb454947a6d7221151
Gerasimenko Mike <github@gerasimenko.me>
2016-12-19 12:32:44 +0100

    Added use of the new data model fetching API

--- Wire-iOS/Sources/UserInterface/Collections/CollectionsViewController.swift
+++ Wire-iOS/Sources/UserInterface/Collections/CollectionsViewController.swift
@@ -18,77 +18,56 @@
 
 
 import Foundation
-
-public struct CollectionsSectionSet: OptionSet {
-    public let rawValue: UInt
-    
-    public init(rawValue: UInt) {
-        self.rawValue = rawValue
-    }
-    
-    public init?(index: UInt) {
-        self = type(of: self).visible[Int(index)]
-    }
-    
-    public static let none = CollectionsSectionSet(rawValue: 0)
-    public static let images = CollectionsSectionSet(rawValue: 1)
-    public static let filesAndAudio = CollectionsSectionSet(rawValue: 1 << 1)
-    public static let videos = CollectionsSectionSet(rawValue: 1 << 2)
-    public static let links = CollectionsSectionSet(rawValue: 1 << 3)
-    
-    public static let all: CollectionsSectionSet = [.images, .filesAndAudio, .videos, .links]
-    
-    public static let visible: [CollectionsSectionSet] = [images, filesAndAudio, videos] // links
-    
-    static func totalVisible() -> UInt {
-        return UInt(self.visible.count)
-    }
-}
+import ZMCDataModel
 
 final public class CollectionsViewController: UIViewController {
-    public let conversation: ZMConversation
     public var onDismiss: ((CollectionsViewController)->())?
     public var analyticsTracker: AnalyticsTracker?
     public let sections: CollectionsSectionSet
     
-    private let collectionsView = CollectionsView()
+    fileprivate let collectionsView = CollectionsView()
     fileprivate let messagePresenter = MessagePresenter()
     
     fileprivate var imageMessages: [ZMConversationMessage] = []
     fileprivate var videoMessages: [ZMConversationMessage] = []
     fileprivate var fileAndAudioMessages: [ZMConversationMessage] = []
     
-    init(conversation: ZMConversation, sections: CollectionsSectionSet = .all) {
-        self.conversation = conversation
+    fileprivate let collection: AssetCollectionHolder
+    
+    convenience init(conversation: ZMConversation) {
+        
+        let assetCollecitonMulticastDelegate = AssetCollectionMulticastDelegate()
+        
+        let include = [MessageCategory.image, MessageCategory.file]
+        let exclude = [MessageCategory.GIF, MessageCategory.video]
+        
+        let assetCollection = AssetCollection(conversation: conversation, including: include, excluding: exclude, delegate: assetCollecitonMulticastDelegate)
+        
+        let holder = AssetCollectionHolder(conversation: conversation, assetCollection: assetCollection, assetCollectionDelegate: assetCollecitonMulticastDelegate)
+        
+        self.init(collection: holder)
+    }
+    
+    init(collection: AssetCollectionHolder, sections: CollectionsSectionSet = .all, messages: [ZMConversationMessage] = []) {
+        self.collection = collection
         self.sections = sections
+        
+        switch(sections) {
+        case CollectionsSectionSet.images:
+            self.imageMessages = messages
+        case CollectionsSectionSet.filesAndAudio:
+            self.fileAndAudioMessages = messages
+        case CollectionsSectionSet.videos:
+            self.videoMessages = messages
+        default: break
+        }
+        
         super.init(nibName: .none, bundle: .none)
-        
-        if sections.contains(.images) {
-            self.imageMessages = self.conversation.messages.filter {
-                if let message = $0 as? ZMConversationMessage, let _ = message.imageMessageData
-                { return true }
-                else
-                { return false }
-            } as! [ZMConversationMessage]
-        }
-        
-        if sections.contains(.videos) {
-            self.videoMessages = self.conversation.messages.filter {
-                if let message = $0 as? ZMConversationMessage, let fileMessageData = message.fileMessageData, fileMessageData.isVideo()
-                { return true }
-                else
-                { return false }
-                } as! [ZMConversationMessage]
-        }
-        
-        if sections.contains(.filesAndAudio) {
-            self.fileAndAudioMessages = self.conversation.messages.filter {
-                if let message = $0 as? ZMConversationMessage, let fileMessageData = message.fileMessageData, !fileMessageData.isVideo()
-                { return true }
-                else
-                { return false }
-                } as! [ZMConversationMessage]
-        }
+        self.collection.assetCollectionDelegate.add(self)
+    }
+    
+    deinit {
+        self.collection.assetCollectionDelegate.remove(self)
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -113,7 +92,7 @@ final public class CollectionsViewController: UIViewController {
     }
     
     private func setupNavigationItem() {
-        self.navigationItem.titleView = ConversationTitleView(conversation: self.conversation, interactive: false)
+        self.navigationItem.titleView = ConversationTitleView(conversation: self.collection.conversation, interactive: false)
         
         let button = CollectionsView.closeButton()
         button.addTarget(self, action: #selector(CollectionsViewController.closeButtonPressed(_:)), for: .touchUpInside)
@@ -155,6 +134,28 @@ final public class CollectionsViewController: UIViewController {
     
     @objc func backButtonPressed(_ button: UIButton) {
         _ = self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension CollectionsViewController: AssetCollectionDelegate {
+    public func assetCollectionDidFetch(collection: ZMCollection, messages: [MessageCategory : [ZMMessage]], hasMore: Bool) {
+        for messageCategory in messages {
+            let conversationMessages = messageCategory.value as [ZMConversationMessage]
+            
+            if messageCategory.key.contains(.image) {
+                self.imageMessages.append(contentsOf: conversationMessages)
+            }
+            
+            if messageCategory.key.contains(.file) {
+                self.fileAndAudioMessages.append(contentsOf: conversationMessages)
+            }
+        }
+        
+        self.collectionsView.collectionView.reloadData()
+    }
+    
+    public func assetCollectionDidFinishFetching(collection: ZMCollection, result: AssetFetchResult) {
+        
     }
 }
 
@@ -284,7 +285,7 @@ extension CollectionsViewController: UICollectionViewDelegate, UICollectionViewD
                 guard let `self` = self else {
                     return
                 }
-                let collectionController = CollectionsViewController(conversation: self.conversation, sections: section)
+                let collectionController = CollectionsViewController(collection: self.collection, sections: section, messages: self.elements(for: section))
                 collectionController.analyticsTracker = self.analyticsTracker
                 collectionController.onDismiss = {
                     _ = $0.navigationController?.popViewController(animated: true)
