25fcb35c9fdcc1a74387bd67a6c36511c1ae60a3
Gerasimenko Mike <github@gerasimenko.me>
2017-01-09 16:30:48 +0100

    Use immutable attributed strings

--- Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController.m
+++ Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController.m
@@ -268,7 +268,7 @@
                         attributes:textAttributes];
     
     self.noConversationLabel = [[UILabel alloc] initForAutoLayout];
-    self.noConversationLabel.attributedText = [attributedString copy];
+    self.noConversationLabel.attributedText = [[NSAttributedString alloc] initWithString:attributedString];
     self.noConversationLabel.numberOfLines = 0;
     [self.contentContainer addSubview:self.noConversationLabel];
 }
