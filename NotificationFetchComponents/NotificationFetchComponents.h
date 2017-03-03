7cd2d9a33f1c34bf4c354dc5a45b6682075c062e
Silvan Dähn <silvandaehn@me.com>
2017-03-03 11:18:37 +0100

    Add notification content fetch engine framework

---
+++ NotificationFetchComponents/NotificationFetchComponents.h
@@ -0,0 +1,19 @@
+//
+//  NotificationFetchComponents.h
+//  NotificationFetchComponents
+//
+//  Created by Silvan Dähn on 03.03.17.
+//  Copyright © 2017 Zeta Project Germany GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotificationFetchComponents.
+FOUNDATION_EXPORT double NotificationFetchComponentsVersionNumber;
+
+//! Project version string for NotificationFetchComponents.
+FOUNDATION_EXPORT const unsigned char NotificationFetchComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationFetchComponents/PublicHeader.h>
+
+
---
+++ NotificationFetchComponentsTests/NotificationFetchComponentsTests.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationFetchComponentsTests.swift
+//  NotificationFetchComponentsTests
+//
+//  Created by Silvan Dähn on 03.03.17.
+//  Copyright © 2017 Zeta Project Germany GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import NotificationFetchComponents
+
+class NotificationFetchComponentsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
