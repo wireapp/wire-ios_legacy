ed18a520c3fe11eb2438c2f4df8f32d9b780f88c
Silvan Dähn <silvandaehn@me.com>
2017-04-04 17:50:21 +0200

    Fix first batch of warnings in main target

--- Wire-iOS/Sources/UserInterface/Sketchpad/CanvasViewController.swift
+++ Wire-iOS/Sources/UserInterface/Sketchpad/CanvasViewController.swift
@@ -359,7 +359,7 @@ extension CanvasViewController : UIImagePickerControllerDelegate {
             }
             picker.dismiss(animated: true, completion: nil)
         }) { (error) in
-            DDLogError("error: \(error)")
+            DDLogError("error: \(String(describing: error))")
         }
     }
     
