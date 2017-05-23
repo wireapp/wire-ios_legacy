7adbb4d63bca2f38f53bfc086ffbdc1cbf91377b
Gerasimenko Mike <mike@gerasimenko.me>
2017-05-23 14:42:37 +0200

    Corrected SE API calls

--- Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+CameraPicker.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+CameraPicker.swift
@@ -102,9 +102,9 @@ extension ConversationListViewController {
     }
     
     public func showShareControllerFor(image: UIImage) {
-        let conversations = ZMConversationList.conversations(inUserSession: ZMUserSession.shared()!, team: ZMUser.selfUser().activeTeam)
+        let conversations = ZMConversationList.conversationsIncludingArchived(inUserSession: ZMUserSession.shared()!, team: ZMUser.selfUser().activeTeam) as! [ZMConversation]
         
-        let shareViewController: ShareViewController<ZMConversation, UIImage> = ShareViewController(shareable: image, destinations: conversations as! [ZMConversation])
+        let shareViewController: ShareViewController<ZMConversation, UIImage> = ShareViewController(shareable: image, destinations: conversations)
         let keyboardAvoiding = KeyboardAvoidingViewController(viewController: shareViewController)
 
         shareViewController.preferredContentSize = CGSize(width: 320, height: 568)
@@ -119,9 +119,9 @@ extension ConversationListViewController {
     }
     
     public func showShareControllerFor(videoAtURL: URL) {
-        let conversations = ZMConversationList.conversations(inUserSession: ZMUserSession.shared()!, team: ZMUser.selfUser().activeTeam)
+        let conversations = ZMConversationList.conversationsIncludingArchived(inUserSession: ZMUserSession.shared()!, team: ZMUser.selfUser().activeTeam) as! [ZMConversation]
         
-        let shareViewController: ShareViewController<ZMConversation, URL> = ShareViewController(shareable: videoAtURL, destinations: conversations as! [ZMConversation])
+        let shareViewController: ShareViewController<ZMConversation, URL> = ShareViewController(shareable: videoAtURL, destinations: conversations)
         let keyboardAvoiding = KeyboardAvoidingViewController(viewController: shareViewController)
 
         shareViewController.preferredContentSize = CGSize(width: 320, height: 568)
