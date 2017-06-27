e2c08d4658fcf5abcd1762019cd46eb5b5b2835a
Silvan Dähn <silvandaehn@me.com>
2017-06-27 14:47:58 +0200

    Wrap resign- / becomeFIrstResponder in performWithoutAnimation block

--- Wire-iOS/Sources/UserInterface/Helpers/UITextView+Autocorrect.swift
+++ Wire-iOS/Sources/UserInterface/Helpers/UITextView+Autocorrect.swift
@@ -22,7 +22,9 @@ import UIKit
 extension UITextView {
     // Autocorrects the last word, if necessary.
     func autocorrectLastWord() {
-        resignFirstResponder()
-        becomeFirstResponder()
+        UITextView.performWithoutAnimation {
+            resignFirstResponder()
+            becomeFirstResponder()
+        }
     }
 }
