c0dd44a341d0d58f887eed15c5b66573e5f550da
John Nguyen <johnxuannguyen@gmail.com>
2017-08-25 11:17:07 +0200

    revert isCallkitSupported condition

--- Wire-iOS/Sources/AppRootViewController.swift
+++ Wire-iOS/Sources/AppRootViewController.swift
@@ -55,7 +55,7 @@ class AppRootViewController : UIViewController {
         let isCallkitEnabled = !Settings.shared().disableCallKit
         var isCallkitSupported = false
         if #available(iOS 10, *) {
-            isCallkitSupported = true
+            isCallkitSupported = TARGET_OS_SIMULATOR != 0
         }
         ZMUserSession.useCallKit = isCallkitEnabled && isCallkitSupported
         
