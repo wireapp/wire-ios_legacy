73d7e86a4b9c7023dcde363cbdd130e7738f7a07
Jacob Persson <jacob.persson@wire.com>
2017-10-18 11:55:33 +0200

    Keep CBR setting disabled for now

--- Wire-iOS/Sources/AppRootViewController.swift
+++ Wire-iOS/Sources/AppRootViewController.swift
@@ -127,7 +127,7 @@ class AppRootViewController : UIViewController {
             self.sessionManager?.localMessageNotificationResponder = self
             self.sessionManager?.requestToOpenViewDelegate = self
             sessionManager.updateCallNotificationStyleFromSettings()
-            sessionManager.useConstantBitRateAudio = Settings.shared().callingConstantBitRate
+            sessionManager.useConstantBitRateAudio = false //  Settings.shared().callingConstantBitRate TODO re-enable
         }
     }
     
