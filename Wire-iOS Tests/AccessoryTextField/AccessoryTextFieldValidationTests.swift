04e5c515b63ac93230a3e3ce080c1bcc741f63a7
Bill Chan <billypchan@gmail.com>
2017-11-29 09:44:29 +0100

    fix Typo

--- Wire-iOS Tests/AccessoryTextField/AccessoryTextFieldValidationTests.swift
+++ Wire-iOS Tests/AccessoryTextField/AccessoryTextFieldValidationTests.swift
@@ -19,7 +19,7 @@
 import XCTest
 @testable import Wire
 
-final class AccessoryTextFieldValidateionTests: XCTestCase {
+final class AccessoryTextFieldValidationTests: XCTestCase {
     var sut: AccessoryTextField!
     var mockViewController: MockViewController!
 
