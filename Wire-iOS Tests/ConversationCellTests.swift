ea0326c4b188917baeb499dd6b685e4660c0cf14
Bill Chan <billypchan@gmail.com>
2017-12-22 14:33:00 +0100

    test for ConversationCell retain issue

---
+++ Wire-iOS Tests/ConversationCellTests.swift
@@ -0,0 +1,68 @@
+//
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+
+import XCTest
+@testable import Wire
+
+final class ConversationCellTests: XCTestCase {
+
+    func testConversationCellCanBeReleased(){
+        // GIVEN
+        weak var sut : ConversationCell?
+
+        // WHEN
+        autoreleasepool{
+            let cellInTable = ConversationCell()
+            sut = cellInTable
+
+            var tableView : UITableView! = cellInTable.wrapInTableView()
+            tableView.reloadData()
+
+            tableView = nil
+        }
+
+        // THEN
+        XCTAssertEqual(sut, nil)
+    }
+
+    func testConversationCellIsNotRetainedAfterTimerIsScheduled(){
+        // GIVEN
+        weak var sut : ConversationCell?
+
+        // WHEN
+        autoreleasepool{
+            let cellInTable = ConversationCell()
+            sut = cellInTable
+
+            let layoutProperties = ConversationCellLayoutProperties()
+            layoutProperties.showBurstTimestamp = true
+            let mockMessage = MockMessageFactory.locationMessage()
+            cellInTable.configure(for: mockMessage, layoutProperties: layoutProperties)
+            var tableView : UITableView! = cellInTable.wrapInTableView()
+            tableView.reloadData()
+
+            /// schedule a timer
+            cellInTable.willDisplayInTableView()
+            tableView = nil
+        }
+
+        // THEN
+        XCTAssertEqual(sut, nil)
+    }
+}
