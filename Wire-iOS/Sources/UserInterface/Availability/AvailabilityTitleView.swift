136c6789cd424f7596dd959b60df8d1123b4914c
Gerasimenko Mike <mike@gerasimenko.me>
2017-12-22 13:17:09 +0100

    Improved haptic feedback, added feedback for calls

--- Wire-iOS/Sources/UserInterface/Availability/AvailabilityTitleView.swift
+++ Wire-iOS/Sources/UserInterface/Availability/AvailabilityTitleView.swift
@@ -88,6 +88,13 @@ import WireDataModel
         self.accessibilityLabel = "\(user.name)_is_\(user.availability.localizedName)".localized
     }
     
+    func provideHapticFeedback() {
+        guard #available(iOS 10, *) else {
+            return
+        }
+        
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    }
 }
 
 extension AvailabilityTitleView: ZMUserObserver {
@@ -95,10 +102,7 @@ extension AvailabilityTitleView: ZMUserObserver {
     public func userDidChange(_ changeInfo: UserChangeInfo) {
         guard changeInfo.availabilityChanged else { return }
         
-        if #available(iOS 10, *) {
-            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
-        }
-        
+        provideHapticFeedback()
         configure()
     }
 }
