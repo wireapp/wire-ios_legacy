eda5964852d19493ac6e892d555997d638039c2d
Bill Chan <billypchan@gmail.com>
2018-01-25 18:25:29 +0100

    undo format clean up

--- Wire-iOS/Sources/UserInterface/StartUI/Common/SectionControllers/ServicesSection.swift
+++ Wire-iOS/Sources/UserInterface/StartUI/Common/SectionControllers/ServicesSection.swift
@@ -16,6 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+
 import Foundation
 
 public class ServicesSection: NSObject, CollectionViewSectionController {
@@ -25,13 +26,13 @@ public class ServicesSection: NSObject, CollectionViewSectionController {
             self.isHidden = services.isEmpty
         }
     }
-
+    
     public var collectionView: UICollectionView! = nil {
         didSet {
             guard let collectionView = self.collectionView else {
                 return
             }
-
+            
             collectionView.register(SearchResultCell.self,
                                     forCellWithReuseIdentifier: ServicesSection.ServicesSectionCellReuseIdentifier)
 
@@ -40,45 +41,46 @@ public class ServicesSection: NSObject, CollectionViewSectionController {
                                     withReuseIdentifier: PeoplePickerHeaderReuseIdentifier)
         }
     }
-
-    public weak var delegate: CollectionViewSectionDelegate?
-
+    
+    public weak var delegate: CollectionViewSectionDelegate? = nil
+    
     public static let ServicesSectionCellReuseIdentifier = "ServicesSectionCellReuseIdentifier"
-
+    
     public var isHidden: Bool = false
-
+    
     public let colorSchemeVariant: ColorSchemeVariant
-
+    
     public init(colorSchemeVariant: ColorSchemeVariant) {
         self.colorSchemeVariant = colorSchemeVariant
         super.init()
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.services.count
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                viewForSupplementaryElementOfKind kind: String,
                                at indexPath: IndexPath) -> UICollectionReusableView {
         guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader,
                                                                                withReuseIdentifier: PeoplePickerHeaderReuseIdentifier,
                                                                                for: indexPath) as? SearchSectionHeaderView else {
-                                                                                fatal("cannot dequeue header")
+            fatal("cannot dequeue header")
         }
-
         headerView.title = "peoplepicker.header.services".localized
+        headerView.clipsToBounds = true
+        headerView.colorSchemeVariant = colorSchemeVariant
         return headerView
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ServicesSection.ServicesSectionCellReuseIdentifier,
                                                             for: indexPath) as? SearchResultCell
-            else {
-                fatal("cannot dequeue cell")
+        else {
+            fatal("cannot dequeue cell")
         }
-
+        
         let user = self.services[indexPath.item]
         cell.user = user
         cell.colorSchemeVariant = colorSchemeVariant
@@ -86,7 +88,7 @@ public class ServicesSection: NSObject, CollectionViewSectionController {
             guard let `self` = self else {
                 return
             }
-
+            
             self.delegate?.collectionViewSectionController(self, didDoubleTapItem: user, at: indexPath)
         }
         cell.instantConnectAction = { [weak self] _ in
@@ -94,35 +96,34 @@ public class ServicesSection: NSObject, CollectionViewSectionController {
         }
         return cell
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: false)
 
         let item = self.services[indexPath.item]
         self.delegate?.collectionViewSectionController(self, didSelectItem: item, at: indexPath)
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         let item = self.services[indexPath.item]
         self.delegate?.collectionViewSectionController(self, didDeselectItem: item, at: indexPath)
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                layout collectionViewLayout: UICollectionViewLayout,
                                referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: self.collectionView.bounds.size.width, height: 40)
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                layout collectionViewLayout: UICollectionViewLayout,
                                sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.collectionView.bounds.size.width, height: 52)
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                layout collectionViewLayout: UICollectionViewLayout,
                                insetForSectionAt section: Int) -> UIEdgeInsets {
         return .zero
     }
 }
-
