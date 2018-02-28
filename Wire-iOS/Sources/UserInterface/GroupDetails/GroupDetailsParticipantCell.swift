088ff04d64f3fbe92364add66a31f317cb3bb130
Jacob Persson <jacob.persson@gmail.com>
2018-02-28 16:32:07 +0100

    Fix cell separator color and update snapshot tests

--- Wire-iOS/Sources/UserInterface/GroupDetails/GroupDetailsParticipantCell.swift
+++ Wire-iOS/Sources/UserInterface/GroupDetails/GroupDetailsParticipantCell.swift
@@ -145,7 +145,7 @@ class GroupDetailsParticipantCell: UICollectionViewCell {
     
     private func configureColors() {
         backgroundColor = UIColor.wr_color(fromColorScheme: ColorSchemeColorBarBackground, variant: variant)
-        separator.backgroundColor = UIColor.wr_color(fromColorScheme: ColorSchemeColorSeparator, variant: variant)
+        separator.backgroundColor = UIColor.wr_color(fromColorScheme: ColorSchemeColorCellSeparator, variant: variant)
         guestIconView.image = UIImage(for: .guest, iconSize: .tiny, color: UIColor.wr_color(fromColorScheme: ColorSchemeColorSectionText, variant: variant))
         accessoryActionButton.setIconColor(UIColor.wr_color(fromColorScheme: ColorSchemeColorSectionText, variant: variant), for: .normal)
         titleLabel.textColor = UIColor.wr_color(fromColorScheme: ColorSchemeColorTextForeground, variant: variant)
