cc9e989365eb8d7cfd95c0c321374019e8af298d
Silvan Dähn <silvandaehn@me.com>
2018-03-13 11:53:27 +0100

    Fix fonts and remove rename option from group details / guest options

--- Wire-iOS/Sources/UserInterface/Conversation Options/ActionCell.swift
+++ Wire-iOS/Sources/UserInterface/Conversation Options/ActionCell.swift
@@ -48,6 +48,7 @@ final class ActionCell: UITableViewCell, CellConfigurationConfigurable {
         button.setIcon(.link, with: .tiny, for: .normal)
         button.setIconColor(.init(for: .strongBlue), for: .normal)
         button.setTitleColor(.init(for: .strongBlue), for: .normal)
+        button.titleLabel?.font = FontSpec(.normal, .light).font
         button.titleImageSpacing = 8
         contentView.addSubview(button)
     }
