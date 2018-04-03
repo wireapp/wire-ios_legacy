c0e98eecfb4214f128d755858c906a61d2e1622d
Nicola Giancecchi <nicola@wire.com>
2018-04-03 09:57:48 +0200

    Moved import to test bridging header

--- Wire-iOS/WireBridgingHeader.h
+++ Wire-iOS/WireBridgingHeader.h
@@ -96,7 +96,6 @@
 #import "ConfirmAssetViewController.h"
 #import "ProfileSelfPictureViewController.h"
 #import "AddEmailPasswordViewController.h"
-#import "EmailVerificationStepViewController.h"
 #import "AddPhoneNumberViewController.h"
 #import "VersionInfoViewController.h"
 #import "SketchColorPickerController.h"
