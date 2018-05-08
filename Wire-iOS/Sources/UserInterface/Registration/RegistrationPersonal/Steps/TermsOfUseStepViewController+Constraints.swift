012eece3d7ff47e2f3105e9ca3c549e2750934f0
Bill Chan <billypchan@gmail.com>
2018-05-08 16:18:10 +0200

    for iPad, containerView matches self.registrationForm().maximumFormSize for iPhone, containerView fill the entire view, agreeButton attaches to its bottom

--- Wire-iOS/Sources/UserInterface/Registration/RegistrationPersonal/Steps/TermsOfUseStepViewController+Constraints.swift
+++ Wire-iOS/Sources/UserInterface/Registration/RegistrationPersonal/Steps/TermsOfUseStepViewController+Constraints.swift
@@ -30,7 +30,6 @@ extension TermsOfUseStepViewController {
         let horizontalInset: CGFloat = 28
 
         constrain(titleLabel, termsOfUseText, agreeButton, containerView) { titleLabel, termsOfUseText, agreeButton, containerView in
-            titleLabel.top == containerView.top + horizontalInset
             titleLabel.right == containerView.right - horizontalInset
             titleLabel.left == containerView.left + horizontalInset
 
@@ -47,6 +46,7 @@ extension TermsOfUseStepViewController {
         if self.isIPadRegular(device: UIDevice.current) {
             constrain(containerView, self.view) { containerView, selfView in
                 containerView.width == self.registrationForm().maximumFormSize.width
+                containerView.height == self.registrationForm().maximumFormSize.height
 
                 containerView.center == selfView.center
             }
