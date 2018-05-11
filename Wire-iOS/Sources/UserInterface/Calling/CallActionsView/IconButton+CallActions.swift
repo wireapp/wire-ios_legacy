93647d5c435071f6a3abb08cbf67eece68113bd6
Silvan Dähn <silvandaehn@me.com>
2018-05-11 15:48:17 +0200

    Fix decline button icon size

--- Wire-iOS/Sources/UserInterface/Calling/CallActionsView/IconButton+CallActions.swift
+++ Wire-iOS/Sources/UserInterface/Calling/CallActionsView/IconButton+CallActions.swift
@@ -35,6 +35,7 @@ extension IconButton {
     static func endCall() -> IconButton {
         return .init(
             icon: .endCall,
+            size: .small,
             accessibilityId: "LeaveCallButton",
             backgroundColor: ZMAccentColor.vividRed.color,
             iconColor: .white
@@ -43,6 +44,7 @@ extension IconButton {
     
     fileprivate convenience init(
         icon: ZetaIconType,
+        size: ZetaIconSize = .tiny,
         accessibilityId: String,
         backgroundColor: UIColor,
         iconColor: UIColor
