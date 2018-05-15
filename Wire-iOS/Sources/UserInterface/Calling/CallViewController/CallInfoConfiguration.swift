704d9b47b5daa68b671c960d7f439ae7f1b4bfaa
Silvan Dähn <silvandaehn@me.com>
2018-05-15 13:36:05 +0200

    Pause self video when entering background

--- Wire-iOS/Sources/UserInterface/Calling/CallViewController/CallInfoConfiguration.swift
+++ Wire-iOS/Sources/UserInterface/Calling/CallViewController/CallInfoConfiguration.swift
@@ -123,7 +123,7 @@ extension CallParticipantState {
     
     var isSendingVideo: Bool {
         switch self {
-        case .connected(videoState: .started), .connected(videoState: .badConnection): return true
+        case .connected(videoState: let state) where state.isSending: return true
         default: return false
         }
     }
