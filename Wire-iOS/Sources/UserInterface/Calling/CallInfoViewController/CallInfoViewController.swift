433ca9aad8d6f4cc29f6af8504c4f3f900a6a2f8
Alexis Aubry <alexis.aubry@wire.com>
2018-05-29 12:32:41 +0200

    Fix VO for minimise button and 1:1 user avatar view

--- Wire-iOS/Sources/UserInterface/Calling/CallInfoViewController/CallInfoViewController.swift
+++ Wire-iOS/Sources/UserInterface/Calling/CallInfoViewController/CallInfoViewController.swift
@@ -128,12 +128,16 @@ final class CallInfoViewController: UIViewController, CallActionsViewDelegate, C
     }
     
     private func updateNavigationItem() {
-        navigationItem.leftBarButtonItem = UIBarButtonItem(
+        let minimizeItem = UIBarButtonItem(
             icon: .downArrow,
             target: self,
             action: #selector(minimizeCallOverlay)
         )
-        navigationItem.leftBarButtonItem?.accessibilityIdentifier = "CallDismissOverlayButton"
+
+        minimizeItem.accessibilityLabel = "call.actions.label.minimize_call".localized
+        minimizeItem.accessibilityIdentifier = "CallDismissOverlayButton"
+
+        navigationItem.leftBarButtonItem = minimizeItem
     }
 
     private func updateState() {
