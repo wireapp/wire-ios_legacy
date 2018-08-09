134b98654e04484fbacc68f4061fc50daa7e4768
Bill Chan <billypchan@gmail.com>
2018-08-09 16:33:00 +0200

    rename

--- Wire-iOS Tests/FullscreenImageViewController/FullscreenImageViewControllerSnapshotTests.swift
+++ Wire-iOS Tests/FullscreenImageViewController/FullscreenImageViewControllerSnapshotTests.swift
@@ -39,7 +39,7 @@ final class FullscreenImageViewControllerSnapshotTests: ZMSnapshotTestCase {
     }
 
     func testThatVeryLargeImageIsLoadedToImageView(){
-        sut = createFullscreenImageViewControllerForTest(imageFileName: "20000px.gif")
+        sut = createFullscreenImageViewControllerForTest(imageFileName: "20000x20000.gif")
 
         verify(view: sut.view)
     }
