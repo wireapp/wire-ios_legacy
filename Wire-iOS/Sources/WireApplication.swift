8f4b35354fea2102cfa6f8e41e0a8ba6eca2788b
John Nguyen <johnxuannguyen@gmail.com>
2018-08-10 10:42:44 +0200

    Remove deprecated methods

--- Wire-iOS/Sources/WireApplication.swift
+++ Wire-iOS/Sources/WireApplication.swift
@@ -20,11 +20,6 @@ import UIKit
 
 @objcMembers public class WireApplication: UIApplication {
     
-    public override func registerUserNotificationSettings(_ notificationSettings: UIUserNotificationSettings) {
-        if AutomationHelper.sharedHelper.skipFirstLoginAlerts || AutomationHelper.sharedHelper.disablePushNotificationAlert { return }
-        super.registerUserNotificationSettings(notificationSettings)
-    }
-        
     override public func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         guard motion == .motionShake else { return }
         DebugAlert.showSendLogsMessage(
