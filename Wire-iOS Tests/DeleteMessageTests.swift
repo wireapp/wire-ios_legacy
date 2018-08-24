e7804a3537b234cb5b6b15bed3fc80408df4bfb7
Bill Chan <billypchan@gmail.com>
2018-08-24 18:23:56 +0200

    DeleteMessageTests

--- Wire-iOS Tests/DeleteMessageTests.swift
+++ Wire-iOS Tests/DeleteMessageTests.swift
@@ -19,6 +19,16 @@
 import XCTest
 @testable import Wire
 
+enum ConversationCellType : Int {
+    case text
+    case textWithRichMedia
+    case image
+    case fileTransfer
+    case ping
+    case systemMessage
+    case count
+}
+
 final class DeleteMessageTests: XCTestCase {
     
     var sut: DeleteMessageTests!
@@ -33,23 +43,76 @@ final class DeleteMessageTests: XCTestCase {
         super.tearDown()
     }
 
+    func message(for conversationType: ConversationCellType) -> ZMConversationMessage? {
+        var message: MockMessage? = nil
 
+        switch conversationType {
+        case .text:
+            message = MockMessageFactory.textMessage(includingRichMedia: false)
+        case .textWithRichMedia:
+            message = MockMessageFactory.textMessage(includingRichMedia: true)
+        case .image:
+            message = MockMessageFactory.imageMessage()
+        case .ping:
+            message = MockMessageFactory.pingMessage()
+        case .fileTransfer:
+            message = MockMessageFactory.fileTransferMessage()
+            message?.fileMessageData?.transferState = .downloaded
+        case .systemMessage:
+            message = MockMessageFactory.systemMessage(with: .missedCall, users: 1, clients: 1)
+        case .count:
+            XCTFail("You can't just give the ConversationCellTypeCOUNT and expect a message!")
+        }
 
-    /// Example checker method which can be reused in different tests
-    ///
-    /// - Parameters:
-    ///   - file: optional, for XCTAssert logging error source
-    ///   - line: optional, for XCTAssert logging error source
-    fileprivate func checkerExample(file: StaticString = #file, line: UInt = #line) {
-        XCTAssert(true, file: file, line: line)
+        return message as Any as? ZMConversationMessage
     }
 
-    func testExample(){
-        // GIVEN
+    func conversationCell(for conversationType: ConversationCellType) -> ConversationCell? {
+        var cell: ConversationCell! = nil
 
-        // WHEN
+        switch conversationType {
+        case .text, .textWithRichMedia:
+            cell = TextMessageCell(style: .default, reuseIdentifier: "test")
+        case .image:
+            cell = ImageMessageCell(style: .default, reuseIdentifier: "test")
+        case .ping:
+            cell = PingCell(style: .default, reuseIdentifier: "test")
+        case .fileTransfer:
+            cell = FileTransferCell(style: .default, reuseIdentifier: "test")
+        case .systemMessage:
+            cell = CannotDecryptCell(style: .default, reuseIdentifier: "test")
+        case .count:
+            XCTFail("You can't just give the .COUNT and expect a cell!")
+        }
 
-        // THEN
-        checkerExample()
+        cell.configure(for: message(for: conversationType), layoutProperties: nil)
+        cell.autoSetDimension(.height, toSize: 60) ///TODO:
+
+        return cell
     }
+
+    func testThatTheExpectedCellsCanBeDeleted() {
+        
+        // can perform action decides if the action will be present in menu, therefor be deletable
+        let textMessageCell: ConversationCell? = conversationCell(for: .text)
+        XCTAssertTrue((textMessageCell?.canPerformAction(#selector(ConversationCell.deleteMessage(_:)), withSender: nil))!)
+        XCTAssertFalse((textMessageCell?.canPerformAction(#selector(ConversationCell.delete(_:)), withSender: nil))!)
+
+        let richMediaMessageCell: ConversationCell? = conversationCell(for: .textWithRichMedia)
+        XCTAssertTrue((richMediaMessageCell?.canPerformAction(#selector(ConversationCell.deleteMessage(_:)), withSender: nil))!)
+        XCTAssertFalse((richMediaMessageCell?.canPerformAction(#selector(ConversationCell.delete(_:)), withSender: nil))!)
+
+        let fileMessageCell: ConversationCell? = conversationCell(for: .fileTransfer)
+        XCTAssertTrue((fileMessageCell?.canPerformAction(#selector(ConversationCell.deleteMessage(_:)), withSender: nil))!)
+        XCTAssertFalse((fileMessageCell?.canPerformAction(#selector(ConversationCell.delete(_:)), withSender: nil))!)
+
+        let pingMessageCell: ConversationCell? = conversationCell(for: .ping)
+        XCTAssertTrue((pingMessageCell?.canPerformAction(#selector(ConversationCell.deleteMessage(_:)), withSender: nil))!)
+        XCTAssertFalse((pingMessageCell?.canPerformAction(#selector(ConversationCell.delete(_:)), withSender: nil))!)
+
+        let imageMessageCell: ConversationCell? = conversationCell(for: .image)
+        XCTAssertTrue((imageMessageCell?.canPerformAction(#selector(ConversationCell.deleteMessage(_:)), withSender: nil))!)
+        XCTAssertFalse((imageMessageCell?.canPerformAction(#selector(ConversationCell.delete(_:)), withSender: nil))!)
+    }
+
 }
