ad2c2219e298932b926f7dcac0fa40c5e819cfd3
Vytis Šibonis <vytis@wire.com>
2018-09-03 16:27:33 +0300

    Added testing string instead of real logs

--- WireExtensionComponents/Utilities/AutomationHelper.swift
+++ WireExtensionComponents/Utilities/AutomationHelper.swift
@@ -235,8 +235,9 @@ extension AutomationHelper {
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [weak self] in
             if UIPasteboard.general.string == AutomationKey.pasteLogsInClipboard.rawValue {
                 // When magic string is detected we should put contents of the first available log to clipboard
-                let allLogStrings = [ZMSLog.currentLog, ZMSLog.previousLog].lazy.compactMap{ $0 }.compactMap { String(data: $0, encoding: .utf8) }
-                UIPasteboard.general.string = allLogStrings.first ?? "No logs found"
+//                let allLogStrings = [ZMSLog.currentLog, ZMSLog.previousLog].lazy.compactMap{ $0 }.compactMap { String(data: $0, encoding: .utf8) }
+//                UIPasteboard.general.string = allLogStrings.first ?? "No logs found"
+                UIPasteboard.general.string = "TESTING TESTING"
             }
             self?.pollClipboardContentsForMagicString()
         }
