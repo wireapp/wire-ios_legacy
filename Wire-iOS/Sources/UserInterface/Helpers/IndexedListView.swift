6556e3e99bf1d403cdff31b3ac7cfc81db650be6
Alexis Aubry <alexis.aubry@wire.com>
2018-09-25 16:39:19 +0200

    Make cell item check reusable + add tests

--- Wire-iOS/Sources/UserInterface/Helpers/IndexedListView.swift
+++ Wire-iOS/Sources/UserInterface/Helpers/IndexedListView.swift
@@ -18,10 +18,20 @@
 
 import UIKit
 
-extension UICollectionView {
+/**
+ * A view that displays a list of cells that can be accessed by their index.
+ */
+protocol IndexedListView {
+    /// The number of sections in the list.
+    var numberOfSections: Int { get }
+    /// The number of cells in the specified section of the list.
+    func numberOfCells(inSection section: Int) -> Int
+}
+
+extension IndexedListView {
 
     /**
-     * Checks whether the collection view contains an item at the given index path.
+     * Checks whether the indexed list view contains an item at the given index path.
      * - parameter indexPath: The index path to check.
      */
 
@@ -29,10 +39,22 @@ extension UICollectionView {
         if indexPath.section < 0 || indexPath.section >= numberOfSections {
             return false
         }
-        if indexPath.row < 0 || indexPath.row >= numberOfItems(inSection: indexPath.section) {
+        if indexPath.row < 0 || indexPath.row >= numberOfCells(inSection: indexPath.section) {
             return false
         }
         return true
     }
 
+}
+
+extension UITableView: IndexedListView {
+    func numberOfCells(inSection section: Int) -> Int {
+        return numberOfRows(inSection: section)
+    }
+}
+
+extension UICollectionView: IndexedListView {
+    func numberOfCells(inSection section: Int) -> Int {
+        return numberOfItems(inSection: section)
+    }
 }
