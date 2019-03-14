b0fd1b3fc4d63bb2ab7d3d6dc40fd95b0095154c
Jacob Persson <jacob.persson@gmail.com>
2019-03-14 17:18:55 +0100

    Correctly unregister sounds

---
+++ Wire-iOS/Sources/Helpers/sound/SoundPreviewPlayer.swift
@@ -0,0 +1,9 @@
+//
+//  SoundPreviewPlayer.swift
+//  Wire-iOS
+//
+//  Created by Jacob Persson on 14.03.19.
+//  Copyright © 2019 Zeta Project Germany GmbH. All rights reserved.
+//
+
+import Foundation
