29002f09d15d0d99fae8fedb5cd8d6557e423f02
Bill Chan <billypchan@gmail.com>
2019-04-05 11:56:30 +0200

    WIP

--- Wire-iOS/Sources/UserInterface/ConversationList/ListContent/ConnectRequestsCell.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/ListContent/ConnectRequestsCell.swift
@@ -17,9 +17,84 @@
 // 
 
 final class ConnectRequestsCell : UICollectionViewCell {
-    let itemView: ConversationListItemView
+    let itemView = ConversationListItemView()
+
+    private var hasCreatedInitialConstraints = false
+    private var currentConnectionRequestsCount: Int = 0
+    private var conversationListObserverToken: Any?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConnectRequestsCell()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private
+    func setupConnectRequestsCell() {
+        clipsToBounds = true
+        addSubview(itemView)
+        updateAppearance()
+
+        if ZMUserSession.shared != nil {
+            conversationListObserverToken = ConversationListChangeInfo.addObserver(self, forList: ZMConversationList.pendingConnectionConversations(inUserSession: ZMUserSession.shared), userSession: ZMUserSession.shared)
+        }
+
+        setNeedsUpdateConstraints()
+    }
+
+    private
+    func updateConstraints() {
+        if !hasCreatedInitialConstraints {
+            hasCreatedInitialConstraints = true
+            itemView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
+        }
+        super.updateConstraints()
+    }
+
+    ///TODO: override
+    func setSelected(_ selected: Bool) {
+        super.setSelected(selected)
+        if IS_IPAD_FULLSCREEN {
+            itemView.selected = self.selected || highlighted
+        }
+    }
+
+    ///TODO: override
+    func setHighlighted(_ highlighted: Bool) {
+        super.setHighlighted(highlighted)
+        if IS_IPAD_FULLSCREEN {
+            itemView.selected = selected || self.highlighted
+        } else {
+            itemView.selected = self.highlighted
+        }
+    }
+
+    private
+    func updateAppearance() {
+        let connectionRequests = ZMConversationList.pendingConnectionConversations(inUserSession: ZMUserSession.shared)
+
+        let newCount: Int = connectionRequests.count
+
+        if newCount != currentConnectionRequestsCount {
+            let connectionUsers = connectionRequests.map(withBlock: { conversation in
+                ///TODO: inject a conversation
+                return (conversation?.connection.to)!
+            })
+
+            currentConnectionRequestsCount = newCount
+            let title = String(format: NSLocalizedString("list.connect_request.people_waiting", comment: ""), newCount)
+            itemView.configure(with: NSAttributedString(string: title), subtitle: NSAttributedString(), users: connectionUsers)
+        }
+    }
+
 }
 
 extension ConnectRequestsCell: ZMConversationListObserver {
-
+    func conversationListDidChange(_ changeInfo: ConversationListChangeInfo) {
+        updateAppearance()
+    }
 }
+
