d195b3c62e33aa45644dee06720150bf7984227c
Bill Chan <billypchan@gmail.com>
2019-04-08 16:32:14 +0200

    clean up

---
+++ Wire-iOS/Sources/UserInterface/ContactsUI/ContactsViewController+Constraints.swift
@@ -0,0 +1,111 @@
+//
+// Wire
+// Copyright (C) 2019 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+extension ContactsViewController {
+
+    @objc
+    func setupLayout() {
+        createTopContainerConstraints()
+
+        [titleLabel,
+         separatorView,
+         tableView,
+         emptyResultsView,
+         inviteOthersButton,
+         noContactsLabel,
+         cancelButton,
+         bottomContainerSeparatorView,
+         bottomContainerView].forEach(){$0.translatesAutoresizingMaskIntoConstraints = false}
+
+        let standardOffset: CGFloat = 24.0
+        let titleLabelConstraints = titleLabel.fitInSuperview(with: EdgeInsets(top: UIScreen.safeArea.top, leading: standardOffset, bottom: standardOffset, trailing: standardOffset), activate: false)
+
+        titleLabelTopConstraint = titleLabelConstraints[.top]
+        titleLabelBottomConstraint = titleLabelConstraints[.bottom]
+
+        var constraints: [NSLayoutConstraint] = titleLabelConstraints.values.map{$0}
+
+        titleLabelHeightConstraint = titleLabel.heightAnchor.constraint(equalToConstant: 44)
+        titleLabelHeightConstraint.isActive = (titleLabel.text?.count ?? 0) > 0
+
+        createSearchHeaderConstraints()
+
+        constraints += separatorView.fitInSuperview(with: EdgeInsets(margin: standardOffset), exclude: [.top, .bottom], activate: false).values.map{$0}
+        constraints += [separatorView.heightAnchor.constraint(equalToConstant: 0.5),
+                        separatorView.bottomAnchor.constraint(equalTo: tableView.topAnchor),
+                        separatorView.bottomAnchor.constraint(equalTo: emptyResultsView.topAnchor)]
+
+        constraints += tableView.fitInSuperview(exclude: [.top, .bottom], activate: false).values.map{$0}
+        constraints += [tableView.bottomAnchor.constraint(equalTo: bottomContainerView.topAnchor)]
+
+        let emptyResultsViewConstraints = emptyResultsView.fitInSuperview(exclude: [.top], activate: false)
+        emptyResultsBottomConstraint = emptyResultsViewConstraints[.bottom]
+        constraints += emptyResultsViewConstraints.values.map{$0}
+
+        constraints += [noContactsLabel.topAnchor.constraint(equalTo: searchHeaderViewController.view.bottomAnchor, constant: standardOffset),
+
+                        noContactsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: standardOffset),
+                        noContactsLabel.pinToSuperview(anchor: .trailing, activate: false)]
+
+
+        let bottomContainerViewConstraints = bottomContainerView.fitInSuperview(exclude: [.top], activate: false)
+        bottomContainerBottomConstraint = bottomContainerViewConstraints[.bottom]
+
+        constraints += bottomContainerViewConstraints.values.map{$0}
+
+        constraints += bottomContainerSeparatorView.fitInSuperview(exclude: [.bottom], activate: false).values.map{$0}
+        constraints += [bottomContainerSeparatorView.heightAnchor.constraint(equalToConstant: 0.5)]
+
+        closeButtonTopConstraint = cancelButton.pinToSuperview(anchor: .top, inset: 16 + UIScreen.safeArea.top, activate: (titleLabel?.text?.count ?? 0) > 0)
+
+        closeButtonBottomConstraint = cancelButton.pinToSuperview(anchor: .bottom, inset: 8, activate: false)
+        closeButtonBottomConstraint.priority = .defaultLow
+
+        closeButtonHeightConstraint = cancelButton.heightAnchor.constraint(equalToConstant: 16)
+
+        constraints += [closeButtonBottomConstraint,
+                        cancelButton.pinToSuperview(anchor: .trailing, inset: 16, activate: false),
+                        cancelButton.widthAnchor.constraint(equalToConstant: 16),
+                        closeButtonHeightConstraint]
+
+        let inviteOthersButtonConstraints = inviteOthersButton.fitInSuperview(with: EdgeInsets(top: standardOffset / 2.0, leading: standardOffset, bottom: standardOffset / 2.0 + UIScreen.safeArea.bottom, trailing: standardOffset), activate: false)
+
+        bottomEdgeConstraint = inviteOthersButtonConstraints[.bottom]
+
+        constraints += inviteOthersButtonConstraints.values.map{$0}
+
+        constraints += [inviteOthersButton.heightAnchor.constraint(equalToConstant: 28)]
+
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    @objc
+    func createBottomButtonConstraints() {
+        bottomButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomButton.fitInSuperview(with: EdgeInsets(margin: 24), exclude:[.top, .bottom])
+        bottomButton.pinToSuperview(axisAnchor: .centerY)
+    }
+
+    @objc
+    func setupTableView() {
+        let bottomContainerHeight: CGFloat = 56.0 + UIScreen.safeArea.bottom
+        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: bottomContainerHeight, right: 0)
+    }
+}
--- Wire-iOS/Sources/UserInterface/ContactsUI/ContactsViewController.m
+++ Wire-iOS/Sources/UserInterface/ContactsUI/ContactsViewController.m
@@ -69,7 +69,6 @@ static NSString* ZMLogTag ZM_UNUSED = @"UI";
     
     [self setupViews];
     [self setupLayout];
-    [self setupLayout2];
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardFrameDidChange:) name:UIKeyboardWillChangeFrameNotification object:nil];
 
@@ -80,72 +79,6 @@ static NSString* ZMLogTag ZM_UNUSED = @"UI";
 
     [self setupStyle];
 }
-
- - (void)setupLayout2
- {
-// [self createTopContainerConstraints];
-//
- CGFloat standardOffset = 24.0f;
-//
-// self.titleLabelTopConstraint = [self.titleLabel autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:UIScreen.safeArea.top];
-// [self.titleLabel autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:standardOffset];
-// [self.titleLabel autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:standardOffset];
-// self.titleLabelBottomConstraint = [self.titleLabel autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:standardOffset];
-//
-// self.titleLabelHeightConstraint = [self.titleLabel autoSetDimension:ALDimensionHeight toSize:44.0f];
-// self.titleLabelHeightConstraint.active = (self.titleLabel.text.length > 0);
-//
-// [self createSearchHeaderConstraints];
-
-// [self.separatorView autoPinEdgeToSuperviewEdge:ALEdgeLeading withInset:standardOffset];
-// [self.separatorView autoPinEdgeToSuperviewEdge:ALEdgeTrailing withInset:standardOffset];
-// [self.separatorView autoSetDimension:ALDimensionHeight toSize:0.5f];
-// [self.separatorView autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:self.tableView];
-//
-// [self.separatorView autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:self.emptyResultsView];
-
-// [self.tableView autoPinEdgeToSuperviewEdge:ALEdgeLeading];
-// [self.tableView autoPinEdgeToSuperviewEdge:ALEdgeTrailing];
-// [self.tableView autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:self.bottomContainerView withOffset:0];
-
-// [self.emptyResultsView autoPinEdgeToSuperviewEdge:ALEdgeLeading];
-// [self.emptyResultsView autoPinEdgeToSuperviewEdge:ALEdgeTrailing];
-// self.emptyResultsBottomConstraint = [self.emptyResultsView autoPinEdgeToSuperviewEdge:ALEdgeBottom];
-
-// [self.noContactsLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.searchHeaderViewController.view withOffset:standardOffset];
-// [self.noContactsLabel autoPinEdge:ALEdgeLeading toEdge:ALEdgeLeading ofView:self.view withOffset:standardOffset];
-// [self.noContactsLabel autoPinEdgeToSuperviewEdge:ALEdgeTrailing];
-
- CGFloat bottomContainerHeight = 56.0f + UIScreen.safeArea.bottom;
- [self.bottomContainerView autoPinEdgeToSuperviewEdge:ALEdgeLeading];
- [self.bottomContainerView autoPinEdgeToSuperviewEdge:ALEdgeTrailing];
- self.bottomContainerBottomConstraint = [self.bottomContainerView autoPinEdgeToSuperviewEdge:ALEdgeBottom];
-
- [self.bottomContainerSeparatorView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeBottom];
- [self.bottomContainerSeparatorView autoSetDimension:ALDimensionHeight toSize:0.5];
-
- self.tableView.contentInset = UIEdgeInsetsMake(0, 0, bottomContainerHeight, 0);
-
- self.closeButtonTopConstraint = [self.cancelButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:16 + UIScreen.safeArea.top];
- self.closeButtonTopConstraint.active = (self.titleLabel.text.length > 0);
-
- [NSLayoutConstraint autoSetPriority:UILayoutPriorityDefaultLow forConstraints:^{
- self.closeButtonBottomConstraint = [self.cancelButton autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:8];
- }];
-
- [self.cancelButton autoPinEdgeToSuperviewEdge:ALEdgeTrailing withInset:16];
- [self.cancelButton autoSetDimension:ALDimensionWidth toSize:16];
- self.closeButtonHeightConstraint = [self.cancelButton autoSetDimension:ALDimensionHeight toSize:16];
-
- [self.inviteOthersButton autoPinEdgeToSuperviewEdge:ALEdgeLeading withInset:standardOffset];
- [self.inviteOthersButton autoPinEdgeToSuperviewEdge:ALEdgeTrailing withInset:standardOffset];
- [self.inviteOthersButton autoSetDimension:ALDimensionHeight toSize:28];
- [self.inviteOthersButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:standardOffset / 2.0];
- self.bottomEdgeConstraint = [self.inviteOthersButton autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset: standardOffset / 2.0 + UIScreen.safeArea.bottom];
-
- }
-
-
 
 - (BOOL)prefersStatusBarHidden
 {
@@ -200,6 +133,8 @@ static NSString* ZMLogTag ZM_UNUSED = @"UI";
     [self.tableView registerClass:[ContactsCell class] forCellReuseIdentifier:ContactsViewControllerCellID];
     [self.tableView registerClass:[ContactsSectionHeaderView class] forHeaderFooterViewReuseIdentifier:ContactsViewControllerSectionHeaderID];
     [self.view addSubview:self.tableView];
+
+    [self setupTableView];
 
     // Empty results view
     self.emptyResultsView = [[ContactsEmptyResultView alloc] init];
