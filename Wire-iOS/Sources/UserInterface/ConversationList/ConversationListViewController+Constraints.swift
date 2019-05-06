937422fb3c1a95f35a237c95fe9feb001d7405e0
Bill Chan <billypchan@gmail.com>
2019-05-06 11:50:46 +0200

    remove purelayout

---
+++ Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+Constraints.swift
@@ -0,0 +1,58 @@
+//
+// Wire
+// Copyright (C) 2019 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+extension ConversationListViewController {
+
+@objc
+func createViewConstraints() {
+    /*
+    conversationListContainer.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero, excludingEdge: ALEdgeTop)
+
+    bottomBarController.view.autoPinEdge(toSuperviewEdge: ALEdgeLeft)
+    bottomBarController.view.autoPinEdge(toSuperviewEdge: ALEdgeRight)
+    bottomBarBottomOffset = bottomBarController.view.autoPinEdge(toSuperviewEdge: ALEdgeBottom)
+
+    networkStatusViewController.createConstraintsInParentController(withBottomView: topBarViewController.view, controller: self)
+
+
+topBarViewController.view.autoPinEdge(toSuperviewEdge: ALEdgeLeft)
+topBarViewController.view.autoPinEdge(toSuperviewEdge: ALEdgeRight)
+topBarViewController.view.autoPinEdge(ALEdgeBottom, toEdge: ALEdgeTop, ofView: conversationListContainer)
+
+contentContainer.bottomAnchor.constraint(equalTo: safeBottomAnchor).isActive = true
+contentContainer.topAnchor.constraint(equalTo: safeTopAnchor).isActive = true
+contentContainer.leadingAnchor.constraint(equalTo: view.safeLeadingAnchor).isActive = true
+contentContainer.trailingAnchor.constraint(equalTo: view.safeTrailingAnchor).isActive = true
+
+noConversationLabel.autoCenterInSuperview()
+noConversationLabel.autoSetDimension(ALDimensionHeight, toSize: 120.0)
+noConversationLabel.autoSetDimension(ALDimensionWidth, toSize: 240.0)
+
+onboardingHint.autoPinEdge(ALEdgeBottom, toEdge: ALEdgeTop, ofView: bottomBarController.view)
+onboardingHint.autoPinEdge(toSuperviewMargin: ALEdgeLeft)
+onboardingHint.autoPinEdge(toSuperviewMargin: ALEdgeRight)
+
+listContentController.view.autoPinEdge(toSuperviewEdge: ALEdgeTop)
+listContentController.view.autoPinEdge(ALEdgeBottom, toEdge: ALEdgeTop, ofView: bottomBarController.view)
+listContentController.view.autoPinEdge(toSuperviewEdge: ALEdgeLeading)
+listContentController.view.autoPinEdge(toSuperviewEdge: ALEdgeTrailing)
+*/
+}
+}
