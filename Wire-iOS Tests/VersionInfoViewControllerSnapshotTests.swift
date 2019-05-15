57ef5faffec022511c89daa3253e9b1b1cc42255
Bill Chan <billypchan@gmail.com>
2019-05-15 16:39:32 +0200

    WIP snapshot an alert

--- Wire-iOS Tests/VersionInfoViewControllerSnapshotTests.swift
+++ Wire-iOS Tests/VersionInfoViewControllerSnapshotTests.swift
@@ -37,10 +37,44 @@ final class VersionInfoViewControllerSnapshotTests: XCTestCase {
     }
 
     func testForInitState(){
+        ///TODO: move to extension
         let snapshotDirectory = ProcessInfo.processInfo.environment["SNAPSHOT_REFERENCE_DIR"]! + "/" + #file
 
         let failure = verifySnapshot(matching: sut, as: .image, snapshotDirectory: snapshotDirectory)
 
         XCTAssertNil(failure, failure ?? "Test passed")
     }
+}
+
+
+final class UIAlertControllerCompanyLoginSnapshotTests: XCTestCase {
+    var sut: UIAlertController!
+    let snapshotDirectory = ProcessInfo.processInfo.environment["SNAPSHOT_REFERENCE_DIR"]! + "/" + #file
+
+    override func setUp() {
+        super.setUp()
+        sut = UIAlertController.companyLogin(prefilledCode: nil, validator: {_ -> Bool in
+            return true
+        }, completion: {_ in })
+
+        //        let systemAlerts = XCUIApplication(bundleIdentifier: "com.apple.springboard").alerts
+        //        if systemAlerts.buttons["Don't Allow"].exists {
+        //            systemAlerts.buttons["Don't Allow"].tap()
+        //        }
+
+//        recordMode = true
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testForAlert(){
+        ///TODO: alert is too tall
+        let failure = verifySnapshot(matching: sut, as: .image, snapshotDirectory: snapshotDirectory)
+
+        XCTAssertNil(failure, failure ?? "Test passed")
+    }
+
 }
