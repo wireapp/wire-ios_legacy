10423c074185638782a5e02dbddbfc552d13ce5d
Bill Chan <billypchan@gmail.com>
2019-06-13 13:32:14 +0200

    clean up

--- Wire-iOS/Sources/Helpers/syncengine/UserClient+RemoveTask.swift
+++ Wire-iOS/Sources/Helpers/syncengine/UserClient+RemoveTask.swift
@@ -69,7 +69,6 @@ private class ClientRemovalObserver: NSObject, ZMClientUpdateObserver {
     private var strongReference: ClientRemovalObserver? = nil
     let userClientToDelete: UserClient
     let controller: UIViewController
-    private var requestPasswordController: RequestPasswordController?
     let completion: ((Error?)->())?
     var credentials: ZMEmailCredentials?
     private var passwordIsNecessaryForDelete: Bool = false
@@ -112,7 +111,7 @@ private class ClientRemovalObserver: NSObject, ZMClientUpdateObserver {
         controller.showLoadingView = false
 
         if !passwordIsNecessaryForDelete {
-            requestPasswordController = controller.requestPassword { newCredentials in
+            controller.requestPassword { newCredentials in
                 guard let emailCredentials = newCredentials,
                     emailCredentials.password?.isEmpty == false else {
                     self.endRemoval(result: ClientRemovalUIError.noPasswordProvided)
