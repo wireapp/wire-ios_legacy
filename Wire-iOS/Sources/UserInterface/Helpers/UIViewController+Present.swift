84a2cf3e81c0cc2f24e942ed551cbc9b429b42dc
Bill Chan <billypchan@gmail.com>
2019-07-02 12:15:26 +0200

    comment

--- Wire-iOS/Sources/UserInterface/Helpers/UIViewController+Present.swift
+++ Wire-iOS/Sources/UserInterface/Helpers/UIViewController+Present.swift
@@ -1,4 +1,3 @@
-
 // Wire
 // Copyright (C) 2019 Wire Swiss GmbH
 //
@@ -19,6 +18,12 @@
 import Foundation
 
 extension UIViewController {
+
+    /// present self from the top most view controller
+    ///
+    /// - Parameters:
+    ///   - flag: true if animated
+    ///   - completion: the completion closure
     func presentTopmost(animated flag: Bool = true,
                         completion: (() -> Void)? = nil) {
         UIApplication.shared.topmostViewController(onlyFullScreen: false)?.present(self, animated: flag, completion: completion)
