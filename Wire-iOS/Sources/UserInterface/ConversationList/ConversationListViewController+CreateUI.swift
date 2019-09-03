ea492becf7419ca8ed747de57400df8f30d65bb3
Bill Chan <billypchan@gmail.com>
2019-09-03 11:57:53 +0200

    WIP wait for delegate converted

--- Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+CreateUI.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+CreateUI.swift
@@ -28,4 +28,62 @@ extension ConversationListViewController {
             initialSyncObserverToken = ZMUserSession.addInitialSyncCompletionObserver(self, userSession: userSession)
         }
     }
+
+    @objc
+    func createNoConversationLabel() {
+        noConversationLabel = UILabel()
+        noConversationLabel.attributedText = NSAttributedString.attributedTextForNoConversationLabel
+        noConversationLabel.numberOfLines = 0
+        contentContainer.addSubview(noConversationLabel)
+    }
+
+    @objc
+    func createBottomBarController() {
+        bottomBarController = ConversationListBottomBarController(delegate: self)
+        bottomBarController.showArchived = true
+        addChild(bottomBarController)
+        conversationListContainer.addSubview(bottomBarController.view)
+        bottomBarController.didMove(toParent: self)
+    }
+
+    @objc
+    func createListContentController() {
+        listContentController = ConversationListContentController()
+        listContentController.collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: contentControllerBottomInset, right: 0)
+        listContentController.contentDelegate = self
+
+        addChild(listContentController)
+        conversationListContainer.addSubview(listContentController.view)
+        listContentController.didMove(toParent: self)
+    }
+
+
+    func createArchivedListViewController() -> ArchivedListViewController? {
+        let archivedViewController = ArchivedListViewController()
+        archivedViewController.delegate = self
+        return archivedViewController
+    }
+
+}
+
+extension NSAttributedString {
+    static var attributedTextForNoConversationLabel: NSAttributedString? {
+        guard let paragraphStyle = NSParagraphStyle.default as? NSMutableParagraphStyle else { return nil }
+        paragraphStyle.paragraphSpacing = 10
+        paragraphStyle.alignment = .center
+
+        let titleAttributes: [NSAttributedString.Key : Any] = [
+                NSAttributedString.Key.foregroundColor: UIColor.white,
+                NSAttributedString.Key.font: UIFont.smallMediumFont,
+                NSAttributedString.Key.paragraphStyle: paragraphStyle
+            ]
+
+        paragraphStyle.paragraphSpacing = 4
+
+        let titleString = "conversation_list.empty.all_archived.message".localized
+
+        let attributedString = NSAttributedString(string: titleString.uppercased(), attributes: titleAttributes)
+
+        return attributedString
+    }
 }
--- Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+Private.h
+++ Wire-iOS/Sources/UserInterface/ConversationList/ConversationListViewController+Private.h
@@ -56,7 +56,6 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)updateBottomBarSeparatorVisibilityWithContentController:(ConversationListContentController *)controller;
 - (void)setStateValue: (ConversationListState)newState;
 
-- (void)createNoConversationLabel;
 - (BOOL)hasArchivedConversations;
 
 @end
