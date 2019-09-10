8ffe32ce8bd5c74a9c1208267276d3a736f5e2de
Bill Chan <billypchan@gmail.com>
2019-09-10 15:35:38 +0200

    clean up

--- Wire-iOS/Sources/UserInterface/ConversationList/Container/ViewModel/ConversationListViewControllerViewModel+StartUIDelegate.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/Container/ViewModel/ConversationListViewControllerViewModel+StartUIDelegate.swift
@@ -39,9 +39,7 @@ extension ConversationListViewController.ViewModel: StartUIDelegate {
     }
 
     func startUI(_ startUI: StartUIViewController!, didSelect conversation: ZMConversation!) {
-        viewController?.setState(.conversationList, animated:true) { 
-            ZClientViewController.shared()?.select(conversation, focusOnView: true, animated: true)
-        }
+        ZClientViewController.shared()?.select(conversation, focusOnView: true, animated: true)
     }
 }
 
