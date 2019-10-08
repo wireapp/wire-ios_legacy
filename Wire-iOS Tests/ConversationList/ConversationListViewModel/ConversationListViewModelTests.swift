792b4578d89692d126fcbb062fa5d2e66a64edad
Bill Chan <billypchan@gmail.com>
2019-10-08 14:43:46 +0200

    clean up

--- Wire-iOS Tests/ConversationList/ConversationListViewModel/ConversationListViewModelTests.swift
+++ Wire-iOS Tests/ConversationList/ConversationListViewModel/ConversationListViewModelTests.swift
@@ -232,7 +232,7 @@ final class ConversationListViewModelTests: XCTestCase {
         sut.folderEnabled = true
         let mockConversation = ZMConversation()
         fillDummyConversations(mockConversation: mockConversation)
-        sut.setCollapsed(sectionIndex: Int(sectionGroups), collapsed: true)///todo
+        sut.setCollapsed(sectionIndex: Int(sectionGroups), collapsed: true)
 
         ///WHEN
         XCTAssert(sut.collapsed(at: Int(sectionGroups)))
