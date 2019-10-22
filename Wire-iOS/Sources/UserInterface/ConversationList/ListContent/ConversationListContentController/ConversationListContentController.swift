529698cb867ca8284069d6bdc8ff584a724c3892
Bill Chan <billypchan@gmail.com>
2019-10-22 16:32:45 +0200

    undo

--- Wire-iOS/Sources/UserInterface/ConversationList/ListContent/ConversationListContentController/ConversationListContentController.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/ListContent/ConversationListContentController/ConversationListContentController.swift
@@ -38,6 +38,10 @@ extension ConversationListContentController {
     func reload() {
         collectionView.reloadData()
         ensureCurrentSelection()
+
+        // we MUST call layoutIfNeeded here because otherwise bad things happen when we close the archive, reload the conv
+        // and then unarchive all at the same time
+        view.layoutIfNeeded()
     }
 
     // MARK: - section header
