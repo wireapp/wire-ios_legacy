8d5c9cd65bc64d54229a4ba4e4e0d8cd26588a33
bill chan <billypchan@gmail.com>
2019-11-11 11:35:19 +0100

    update tests

--- Wire-iOS Tests/Color/ColorComponentsTests.swift
+++ Wire-iOS Tests/Color/ColorComponentsTests.swift
@@ -50,7 +50,17 @@ final class ColorComponentsTests: XCTestCase {
         XCTAssertNotEqual(UIColor.black, UIColor(red: 0, green: 0, blue: 0, alpha: 1))
     }
 
-    func testThatColorisComparableWithEqualSign() {
+    func testThatColorIsComparableWithEqualSign() {
         XCTAssert(UIColor.black == UIColor(red: 0, green: 0, blue: 0, alpha: 1))
+    }
+
+    func testThatColorCanBeInitializeWithRGBAValues() {
+        let sut = UIColor(rgba:(255, 0, 0, 1))
+        XCTAssertEqual(UIColor.red, sut)
+    }
+
+    func testThatColorCanBeInitializeWithRGBValues() {
+        let sut = UIColor(rgb:(127.5, 0, 127.5))
+        XCTAssertEqual(UIColor.purple, sut)
     }
 }
