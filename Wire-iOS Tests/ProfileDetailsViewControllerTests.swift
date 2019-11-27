680edda29d60e23d53020c08b075a9cabb2cffcc
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2019-11-27 16:56:03 +0200

    Update Wire-iOS Tests/ProfileDetailsViewControllerTests.swift
    Co-Authored-By: Bill Chan <billypchan@gmail.com>

--- Wire-iOS Tests/ProfileDetailsViewControllerTests.swift
+++ Wire-iOS Tests/ProfileDetailsViewControllerTests.swift
@@ -20,7 +20,7 @@ import SnapshotTesting
 import XCTest
 @testable import Wire
 
-final class ProfileDetailsViewControllerTests: ZMSnapshotTestCase {
+final class ProfileDetailsViewControllerTests: XCTestCase {
 
     var selfUserTeam: UUID!
     var selfUser: MockUser!
