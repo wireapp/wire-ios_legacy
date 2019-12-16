c5b4f083bb3c28991def929fa510235e3c3267ba
KaterinaWire <katerina@wire.com>
2019-12-16 16:40:49 +0100

    Fix tests

--- Wire-iOS Tests/Mocks/MockUser.h
+++ Wire-iOS Tests/Mocks/MockUser.h
@@ -52,6 +52,7 @@
 @property (nonatomic, readwrite) BOOL canModifyNotificationSettingsInConversation;
 @property (nonatomic, readwrite) BOOL canModifyAccessControlSettingsInConversation;
 @property (nonatomic, readwrite) BOOL canAddUserToConversation;
+@property (nonatomic, readwrite) BOOL canModifyOtherMemberInConversation;
 @property (nonatomic, readwrite) BOOL canRemoveUserFromConversation;
 @property (nonatomic, readwrite) BOOL untrusted;
 @property (nonatomic, readwrite) BOOL trusted;
