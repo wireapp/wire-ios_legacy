c8c3b7da238721a2cb6155b6de3604cd85b21349
bill chan <billypchan@gmail.com>
2019-12-18 16:32:53 +0100

    clean up

--- Wire-iOS Tests/StartedConversationCellTests.swift
+++ Wire-iOS Tests/StartedConversationCellTests.swift
@@ -178,7 +178,6 @@ final class StartedConversationCellTests: ConversationCellSnapshotTestCase {
     func testThatItRendersNewConversationCellWithoutParticipants_AllowGuests() {
         teamTest {
             let message = cell(for: .newConversation, text: "Italy Trip", allowGuests: true)
-            (message as! ZMSystemMessage).numberOfGuestsAdded = 10 ///Hack for unstable test
             verify(message: message)
         }
     }
