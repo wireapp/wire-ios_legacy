006d3accd1a2008736a7d9bba2bd17c8d7d7ee82
bill chan <billypchan@gmail.com>
2020-01-27 15:29:20 +0100

    lint

--- Wire-iOS/Sources/UserInterface/Helpers/UIViewController+isInsidePopover.swift
+++ Wire-iOS/Sources/UserInterface/Helpers/UIViewController+isInsidePopover.swift
@@ -17,12 +17,11 @@
 // 
 
 extension UIViewController {
-    
+
     /// Returns true if the view controller is presented inside a popover
     var isInsidePopover: Bool {
         guard let popoverPresentationController = popoverPresentationController else { return false }
-        
+
         return popoverPresentationController.arrowDirection != .unknown
     }
 }
-    
