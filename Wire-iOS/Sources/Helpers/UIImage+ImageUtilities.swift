e7214472b046505fd40ad2ba97dec4f635a856cd
bill chan <billypchan@gmail.com>
2020-01-31 17:48:32 +0100

    clean up

--- Wire-iOS/Sources/Helpers/UIImage+ImageUtilities.swift
+++ Wire-iOS/Sources/Helpers/UIImage+ImageUtilities.swift
@@ -64,6 +64,7 @@ extension UIImage {
         return colorImage
     }
     
+    @objc(singlePixelImageWithColor:)
     class func singlePixelImage(with color: UIColor) -> UIImage? {
         let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
         UIGraphicsBeginImageContext(rect.size)
