8563f8b013eda0f034551e4310592365ce4e46e4
bill chan <billypchan@gmail.com>
2020-01-31 16:09:35 +0100

    WIP

--- Wire-iOS/Sources/Helpers/UIImage+ImageUtilities.swift
+++ Wire-iOS/Sources/Helpers/UIImage+ImageUtilities.swift
@@ -19,7 +19,6 @@
 import Foundation
 
 extension UIImage {
-    @objc(imageScaledWithFactor:)
     func imageScaled(with scaleFactor: CGFloat) -> UIImage? {
         let size = self.size.applying(CGAffineTransform(scaleX: scaleFactor, y: scaleFactor))
         let scale: CGFloat = 0 // Automatically use scale factor of main screens
@@ -31,5 +30,160 @@ extension UIImage {
         UIGraphicsEndImageContext()
 
         return scaledImage
+    }
+    
+    func desaturatedImage(with context: CIContext?, saturation: NSNumber?) -> UIImage? {
+        var i: CIImage? = nil
+        if let cg = self.cgImage {
+            i = CIImage(cgImage: cg)
+        }
+        var filter = CIFilter(name: "CIColorControls")
+        filter?.setValue(i, forKey: kCIInputImageKey)
+        filter?.setValue(saturation, forKey: "InputSaturation")
+        let result = filter?.value(forKey: kCIOutputImageKey) as? CIImage
+        var cgImage: CGImage? = nil
+        if let result = result {
+            cgImage = context?.createCGImage(result, from: result?.extent ?? CGRect.zero)
+        }
+        var processed: UIImage? = nil
+        if let cgImage = cgImage {
+            processed = UIImage(cgImage: cgImage, scale: scale, orientation: imageOrientation)
+        }
+        CGImageRelease(cgImage)
+        
+        return processed
+    }
+
+    ///TODO: init
+    func image(with insets: UIEdgeInsets, backgroundColor: UIColor?) -> UIImage? {
+        let newSize = CGSize(width: size.width + insets.left + insets.right, height: size.height + insets.top + insets.bottom)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, _: 0.0 != 0, _: 0.0)
+        
+        backgroundColor?.setFill()
+        UIRectFill(CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        draw(in: CGRect(x: insets.left, y: insets.top, width: size.width, height: size.height))
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return colorImage
+    }
+
+    class func singlePixelImage(with color: UIColor?) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setFillColor(color?.cgColor)
+        context?.fill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+
+    class func deviceOptimizedImage(from imageData: Data?) -> UIImage? {
+        return self.image(from: imageData, withMaxSize: UIScreen.main.nativeBounds.size.height)
+    }
+
+    convenience init(fromData imageData: Data, withMaxSize maxSize: CGFloat) {
+        if imageData == nil {
+            return nil
+        }
+        
+        var source: CGImageSource? = nil
+        if let data = imageData as? CFData? {
+            source = CGImageSourceCreateWithData(data, nil)
+        }
+        if source == nil {
+            return nil
+        }
+        
+        let options = self.thumbnailOptions(withMaxSize: maxSize)
+        
+        var scaledImage: CGImage? = nil
+        if let source = source {
+            scaledImage = CGImageSourceCreateThumbnailAtIndex(source, 0, options)
+        }
+        if scaledImage == nil {
+            return nil
+        }
+        
+        var image: UIImage? = nil
+        if let scaledImage = scaledImage {
+            image = UIImage(cgImage: scaledImage, scale: 2.0, orientation: .up)
+        }
+        
+        return image
+    }
+    
+    private class func thumbnailOptions(withMaxSize maxSize: CGFloat) -> CFDictionary? {
+        return [
+            kCGImageSourceCreateThumbnailWithTransform : kCFBooleanTrue,
+            kCGImageSourceCreateThumbnailFromImageIfAbsent : kCFBooleanTrue,
+            kCGImageSourceCreateThumbnailFromImageAlways : kCFBooleanTrue,
+            kCGImageSourceThumbnailMaxPixelSize : NSNumber(value: maxSize)
+            ] as? CFDictionary?
+    }
+    
+
+    convenience init(fromData imageData: Data, withShorterSideLength shorterSideLength: CGFloat) {
+            if imageData == nil {
+                return nil
+            }
+            
+            var source: CGImageSource? = nil
+            if let data = imageData as? CFData? {
+                source = CGImageSourceCreateWithData(data, nil)
+            }
+            if source == nil {
+                return nil
+            }
+            
+            let size = self.size(for: source)
+            if size.width <= 0 || size.height <= 0 {
+                return nil
+            }
+            
+            var longSideLength = shorterSideLength
+            
+            if size.width > size.height {
+                longSideLength = shorterSideLength * (size.width / size.height)
+            } else if size.height > size.width {
+                longSideLength = shorterSideLength * (size.height / size.width)
+            }
+            
+            let options = self.thumbnailOptions(withMaxSize: longSideLength)
+            
+            var scaledImage: CGImage? = nil
+            if let source = source {
+                scaledImage = CGImageSourceCreateThumbnailAtIndex(source, 0, options)
+            }
+            if scaledImage == nil {
+                return nil
+            }
+            
+            var image: UIImage? = nil
+            if let scaledImage = scaledImage {
+                image = UIImage(cgImage: scaledImage, scale: 2.0, orientation: .up)
+            }
+            
+            return image
+        }
+
+    convenience init(color: UIColor, andSize size: CGSize) {
+        let rect = CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
     }
 }
