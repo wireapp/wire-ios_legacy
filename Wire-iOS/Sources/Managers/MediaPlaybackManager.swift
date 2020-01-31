d74831a7dea43ed6feaf4d9e5fa746597e3e3270
bill chan <billypchan@gmail.com>
2020-01-31 14:34:17 +0100

    titleObserver

--- Wire-iOS/Sources/Managers/MediaPlaybackManager.swift
+++ Wire-iOS/Sources/Managers/MediaPlaybackManager.swift
@@ -35,10 +35,12 @@ extension Notification.Name {
 /// This object is an interface for AVS to control conversation media playback
 final class MediaPlaybackManager: NSObject, AVSMedia {
     var audioTrackPlayer: AudioTrackPlayer = AudioTrackPlayer()
+    
     @objc
     dynamic private(set) weak var activeMediaPlayer: MediaPlayer?
+    
     weak var changeObserver: MediaPlaybackManagerChangeObserver?
-    private var titleObserver: NSObject?
+    private var titleObserver: NSKeyValueObservation?
     var name: String!
     
     weak var delegate: AVSMediaDelegate?
@@ -115,16 +117,18 @@ final class MediaPlaybackManager: NSObject, AVSMedia {
     
     // MARK: - Active Media Player State
     private func startObservingMediaPlayerChanges() {
-        titleObserver = KeyValueObserver.observe(activeMediaPlayer as Any, keyPath: "title", target: self, selector: #selector(activeMediaPlayerTitleChanged(_:)), options: [.initial, .new])
+        setObserver(activeMediaPlayer as? NSObject & MediaPlayer)
+        
+    }
+    
+    func setObserver<T>(_ mediaPlayerObject: T?) where T: NSObject & MediaPlayer {
+        titleObserver = mediaPlayerObject?.observe(\.title, options: [.initial, .new]) { [weak self] _, _ in
+            self?.changeObserver?.activeMediaPlayerTitleDidChange()
+        }
     }
     
     private func stopObservingMediaPlayerChanges(_ mediaPlayer: MediaPlayer?) {
         titleObserver = nil
-    }
-    
-    @objc
-    private func activeMediaPlayerTitleChanged(_ change: [AnyHashable : Any]?) {
-        changeObserver?.activeMediaPlayerTitleDidChange()
     }
 }
 
