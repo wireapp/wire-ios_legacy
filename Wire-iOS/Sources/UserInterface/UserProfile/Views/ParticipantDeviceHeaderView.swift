588b3a3354ce1a64be99f725c9d3d99dfa8d8bf7
bill chan <billypchan@gmail.com>
2020-02-03 11:35:00 +0100

    converted

--- Wire-iOS/Sources/UserInterface/UserProfile/Views/ParticipantDeviceHeaderView.swift
+++ Wire-iOS/Sources/UserInterface/UserProfile/Views/ParticipantDeviceHeaderView.swift
@@ -18,11 +18,53 @@
 
 import Foundation
 
-extension ParticipantDeviceHeaderView {
+protocol ParticipantDeviceHeaderViewDelegate: class {
+    func participantsDeviceHeaderViewDidTapLearnMore(_ headerView: ParticipantDeviceHeaderView)
+}
+
+final class ParticipantDeviceHeaderView: UIView {
+    private var font: UIFont = .normalLightFont
+    private var textColor: UIColor = .from(scheme: .textForeground)
+    private var linkAttributeColor: UIColor = .accent()
+    private var textView: WebLinkTextView = WebLinkTextView()
+    let userName: String
+    
+    weak var delegate: ParticipantDeviceHeaderViewDelegate?
+    var showUnencryptedLabel = false {
+        didSet {
+            textView.attributedText = attributedExplanationText(for: userName, showUnencryptedLabel: showUnencryptedLabel)
+        }
+    }
+    
+    init(userName: String) {
+        self.userName = userName
+
+        super.init(frame: CGRect.zero)
+        
+        setup()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        backgroundColor = .clear
+        setupViews()
+        setupConstraints()
+    }
+    
+    func setupViews() {
+        textView.textContainer.maximumNumberOfLines = 0
+        textView.delegate = self
+        textView.linkTextAttributes = [:]
+        
+        addSubview(textView)
+    }
     
     //MARK: Attributed Text
     
-    @objc(attributedExplanationTextForUserName:showUnencryptedLabel:)
     func attributedExplanationText(for userName: String,
                                    showUnencryptedLabel unencrypted: Bool) -> NSAttributedString? {
         if unencrypted {
@@ -68,4 +110,18 @@ extension ParticipantDeviceHeaderView {
         return paragraphStyle
     }
     
+    private func setupConstraints() {
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.fitInSuperview(with: EdgeInsets(top: 16, leading: 24, bottom: 16, trailing: 24))
+    }
+
+}
+
+extension ParticipantDeviceHeaderView: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        delegate?.participantsDeviceHeaderViewDidTapLearnMore(self)
+        
+        return false
+    }
 }
