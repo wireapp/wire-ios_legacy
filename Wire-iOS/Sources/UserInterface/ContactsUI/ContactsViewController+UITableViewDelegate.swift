0077484830f13b4eaced60fe2b13a8329684655b
John Nguyen <polyxo@protonmail.com>
2020-02-06 14:27:47 +0100

    Clean up

--- Wire-iOS/Sources/UserInterface/ContactsUI/ContactsViewController+UITableViewDelegate.swift
+++ Wire-iOS/Sources/UserInterface/ContactsUI/ContactsViewController+UITableViewDelegate.swift
@@ -19,26 +19,20 @@
 import Foundation
 
 extension ContactsViewController: UITableViewDelegate {
+
     func headerTitle(section: Int) -> String? {
         return dataSource.tableView(tableView, titleForHeaderInSection: section)
     }
 
     public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let title = headerTitle(section: section), title.count > 0 else {
-            return nil
-        }
-
+        guard let title = headerTitle(section: section), title.count > 0 else { return nil }
         let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: ContactsSectionHeaderView.reuseIdentifier) as? ContactsSectionHeaderView
         headerView?.label.text = title
-
         return headerView
     }
 
     public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        guard let title = headerTitle(section: section), title.count > 0 else {
-            return 0
-        }
-
+        guard let title = headerTitle(section: section), title.count > 0 else { return 0 }
         return ContactsSectionHeaderView.height
     }
 
