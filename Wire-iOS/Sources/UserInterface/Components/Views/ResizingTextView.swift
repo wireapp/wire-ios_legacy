7d378596749b725c0e896cabb8976452a839c036
bill chan <billypchan@gmail.com>
2020-02-07 14:23:36 +0100

    clean up

--- Wire-iOS/Sources/UserInterface/Components/Views/ResizingTextView.swift
+++ Wire-iOS/Sources/UserInterface/Components/Views/ResizingTextView.swift
@@ -16,23 +16,22 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-
 class ResizingTextView: TextView {
     override var contentSize: CGSize {
         didSet {
             invalidateIntrinsicContentSize()
         }
     }
-    
+
     override var intrinsicContentSize: CGSize {
         get {
             return sizeThatFits(CGSize(width: bounds.size.width, height: UIView.noIntrinsicMetric))
         }
     }
-    
+
     override func paste(_ sender: Any?) {
         super.paste(sender)
-        
+
         // Work-around for text view scrolling too far when pasting text smaller
         // than the maximum height of the text view.
         setContentOffset(CGPoint(x: 0, y: 0), animated: false)
