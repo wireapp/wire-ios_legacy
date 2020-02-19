f6114a45d4855fbee468392506fd6aa7779f94a8
Bill Chan <billypchan@gmail.com>
2020-02-19 10:34:17 +0100

    Convert CrossfadeTransition (#4091)
    * WIP
    
    * convert caller
    
    * clean up
    
    * tested on iPad
    
    * lint

--- Wire-iOS/Sources/UserInterface/Components/Controllers/SplitViewController.swift
+++ Wire-iOS/Sources/UserInterface/Components/Controllers/SplitViewController.swift
@@ -39,8 +39,36 @@ extension SplitViewController {
         } else if layoutSize == .regularLandscape {
             return SwizzleTransition(direction: .horizontal)
         }
-        
+
         return CrossfadeTransition()
     }
 
+    @objc
+    func setLeftViewController(_ leftViewController: UIViewController?,
+                               animated: Bool,
+                               transition: SplitViewControllerTransition,
+                               completion: Completion?) {
+        if self.leftViewController == leftViewController {
+            completion?()
+            return
+        }
+
+        let removedViewController = self.leftViewController
+
+        let animator: UIViewControllerAnimatedTransitioning
+
+        if removedViewController == nil || leftViewController == nil {
+            animator = CrossfadeTransition()
+        } else if transition == .present {
+            animator = VerticalTransition(offset: 88)
+        } else if transition == .dismiss {
+            animator = VerticalTransition(offset: -88)
+        } else {
+            animator = CrossfadeTransition()
+        }
+
+        if self.transition(from: removedViewController, to: leftViewController, containerView: leftView, animator: animator, animated: animated, completion: completion) {
+            self.setInternalLeft(leftViewController)
+        }
+    }
 }
