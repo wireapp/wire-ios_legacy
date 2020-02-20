3ab4fbfd9076265b771199993382a1a8b2369f1f
bill chan <billypchan@gmail.com>
2020-02-20 17:24:37 +0100

    convert enum

--- Wire-iOS/Sources/UserInterface/Helpers/UIView+ExtendedBlockAnimations.swift
+++ Wire-iOS/Sources/UserInterface/Helpers/UIView+ExtendedBlockAnimations.swift
@@ -18,6 +18,34 @@
 
 import Foundation
 
+enum EasingFunction {
+    case linear
+    case easeInSine
+    case easeOutSine
+    case easeInOutSine
+    case easeInQuad
+    case easeOutQuad
+    case easeInOutQuad
+    case easeInCubic
+    case easeOutCubic
+    case easeInOutCubic
+    case easeInQuart
+    case easeOutQuart
+    case easeInOutQuart
+    case easeInQuint
+    case easeOutQuint
+    case easeInOutQuint
+    case easeInExpo
+    case easeOutExpo
+    case easeInOutExpo
+    case easeInCirc
+    case easeOutCirc
+    case easeInOutCirc
+    case easeInBack
+    case easeOutBack
+    case easeInOutBack
+}
+
 extension UIView {
     class func animate(easing: EasingFunction,
                        duration: TimeInterval,
