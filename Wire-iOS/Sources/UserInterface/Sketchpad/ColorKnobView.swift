c25d4d48625faa85f0f6f07680111be4904155da
Bill Chan <billypchan@gmail.com>
2020-02-24 19:04:11 +0100

    WIP

--- Wire-iOS/Sources/UserInterface/Sketchpad/ColorKnobView.swift
+++ Wire-iOS/Sources/UserInterface/Sketchpad/ColorKnobView.swift
@@ -20,76 +20,63 @@
 final class ColorKnobView: UIView {
     var isSelected = false {
         didSet {
-            borderCircleLayer.borderColor = knobBorderColor()?.cgColor
-            borderCircleLayer.borderWidth = selected ? 1.0 : 0.0
+            borderCircleLayer.borderColor = knobBorderColor?.cgColor
+            borderCircleLayer.borderWidth = isSelected ? 1.0 : 0.0
         }
     }
+    
     var knobColor: UIColor? {
         didSet {
-            innerCircleLayer.backgroundColor = knobFillColor()?.cgColor
-            innerCircleLayer.borderColor = knobBorderColor()?.cgColor
-            borderCircleLayer.borderColor = knobBorderColor()?.cgColor
+            innerCircleLayer.backgroundColor = knobColor?.cgColor
+            innerCircleLayer.borderColor = knobBorderColor?.cgColor
+            borderCircleLayer.borderColor = knobBorderColor?.cgColor
         }
     }
 
-    var knobDiameter: CGFloat = 0.0
+    var knobDiameter: CGFloat = 6
 
     /// The actual circle knob, filled with the color
-    private var innerCircleLayer: CALayer?
+    private var innerCircleLayer: CALayer = CALayer()
     /// Just a layer, used for the thin border around the selected knob
-    private var borderCircleLayer: CALayer?
+    private var borderCircleLayer: CALayer = CALayer()
     
     init() {
-        super.init()
-        knobDiameter = 6
+        super.init(frame: .zero)
         
-        let innerCircleLayer = CALayer()
-        self.innerCircleLayer = innerCircleLayer
-        if let innerCircleLayer = self.innerCircleLayer {
             layer.addSublayer(innerCircleLayer)
-        }
         
-        let borderCircleLayer = CALayer()
-        self.borderCircleLayer = borderCircleLayer
-        if let borderCircleLayer = self.borderCircleLayer {
             layer.addSublayer(borderCircleLayer)
-        }
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
         let frame = self.frame
-        let centerPos = [frame.size.width / 2, frame.size.height / 2]
+        let centerPos = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
         
         let knobDiameter: CGFloat = self.knobDiameter + 1
-        innerCircleLayer.bounds = [
-            [0, 0],
-            [knobDiameter, knobDiameter]
-        ]
+        innerCircleLayer.bounds = CGRect(origin: .zero, size: CGSize(width: knobDiameter, height: knobDiameter))
         innerCircleLayer.position = centerPos
         innerCircleLayer.cornerRadius = knobDiameter / 2
         innerCircleLayer.borderWidth = 1.0
         
         let knobBorderDiameter = knobDiameter + 6.0
-        borderCircleLayer.bounds = [
-            [0, 0],
-            [knobBorderDiameter, knobBorderDiameter]
-        ]
+        borderCircleLayer.bounds = CGRect(origin: .zero, size: CGSize(width: knobBorderDiameter, height: knobBorderDiameter))
         borderCircleLayer.position = centerPos
         borderCircleLayer.cornerRadius = knobBorderDiameter / 2
     }
     
     // MARK: - Helpers
-    func knobBorderColor() -> UIColor? {
-        if (knobColor == UIColor.white && ColorScheme.default().variant == ColorSchemeVariantLight) || (knobColor == UIColor.black && ColorScheme.default().variant == ColorSchemeVariantDark) {
-            return UIColor.lightGray
+    var knobBorderColor: UIColor? {
+        if (knobColor == .white && ColorScheme.default.variant == .light) ||
+           (knobColor == .black && ColorScheme.default.variant == .dark) {
+            return .lightGray
         }
         return knobColor
     }
-    
-    func knobFillColor() -> UIColor? {
-        return knobColor
-    }
-
 }
