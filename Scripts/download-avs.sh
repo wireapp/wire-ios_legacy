834a9ae607b5cffa503585467325297ad6fcb77d
John Nguyen <polyxo@protonmail.com>
2020-03-09 17:54:57 +0100

    Check if git is installed

--- Scripts/download-avs.sh
+++ Scripts/download-avs.sh
@@ -30,10 +30,13 @@ AVS_FRAMEWORK_NAME="avs.framework"
 # CREDENTIALS
 ##################################
 # prepare credentials if needed
-GITHUB_USERNAME="`git config user.email`"
 
-if [[ -n "${GITHUB_ACCESS_TOKEN}" ]] && [[ -n "${GITHUB_USERNAME}" ]]; then
-  CREDENTIALS="${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}"
+# if git is installed
+if command -v git >dev/null; then
+  GITHUB_USERNAME="`git config user.email`"
+  if [[ -n "${GITHUB_ACCESS_TOKEN}" ]] && [[ -n "${GITHUB_USERNAME}" ]]; then
+    CREDENTIALS="${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}"
+  fi
 fi
 
 ##################################
