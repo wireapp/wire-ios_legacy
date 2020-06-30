357fae30ffdf19d22b25713e1c3eee3df8374295
bill chan <billypchan@gmail.com>
2020-06-30 13:07:46 +0200

    comment

--- Wire-iOS/Sources/UserInterface/Helpers/UIView+Menu.swift
+++ Wire-iOS/Sources/UserInterface/Helpers/UIView+Menu.swift
@@ -24,6 +24,7 @@ extension UIView {
     /// The reason why we are touching the window here is to workaround a bug where,
     /// We now force the window to be the key window and to be the first responder to ensure that we can
     /// show the menu controller.
+    /// ref: https://stackoverflow.com/questions/59176844/uimenucontroller-is-not-visible-in-ios-13-2/62578001#62578001
     func prepareShowingMenu() {
         window?.makeKey()
         window?.becomeFirstResponder()
