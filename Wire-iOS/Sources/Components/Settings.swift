765466865e7b8d18bfd28a67a870b5673822a73f
David Henner <david.henner@wire.com>
2020-07-31 10:23:44 +0200

    replace keyword

--- Wire-iOS/Sources/Components/Settings.swift
+++ Wire-iOS/Sources/Components/Settings.swift
@@ -73,8 +73,8 @@ final class Settings {
     // MARK: - subscript
     subscript<T>(index: SettingKey) -> T? {
         get {
-            if case .conferenceCalling = index, let override = AutomationHelper.sharedHelper.useConferenceCalling as? T {
-               return override
+            if case .conferenceCalling = index, let overrideSetting = AutomationHelper.sharedHelper.useConferenceCalling as? T {
+               return overrideSetting
             }
 
             return defaults.value(forKey: index.rawValue) as? T
