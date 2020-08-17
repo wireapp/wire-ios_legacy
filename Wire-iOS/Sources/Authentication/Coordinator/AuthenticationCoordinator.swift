d49196e081863fc47ed59ea9c54a8a88c75b2f79
bill chan <billypchan@gmail.com>
2020-08-17 19:00:44 +0200

    lint

--- Wire-iOS/Sources/Authentication/Coordinator/AuthenticationCoordinator.swift
+++ Wire-iOS/Sources/Authentication/Coordinator/AuthenticationCoordinator.swift
@@ -41,7 +41,6 @@ class AuthenticationCoordinator: NSObject, AuthenticationEventResponderChainDele
     /// The object receiving updates from the authentication state and providing state.
     weak var delegate: AuthenticationCoordinatorDelegate?
 
-
     // MARK: - Event Handling Properties
 
     /**
@@ -174,7 +173,7 @@ extension AuthenticationCoordinator: AuthenticationStateControllerDelegate {
 // MARK: - Event Handling
 
 extension AuthenticationCoordinator: AuthenticationActioner, SessionManagerCreatedSessionObserver {
-    
+
     func sessionManagerCreated(userSession: ZMUserSession) {
         log.info("Session manager created session: \(userSession)")
         currentPostRegistrationFields().apply(sendPostRegistrationFields)
@@ -191,7 +190,7 @@ extension AuthenticationCoordinator: AuthenticationActioner, SessionManagerCreat
             PostLoginAuthenticationNotification.addObserver(self),
             sessionManager.addSessionManagerCreatedSessionObserver(self)
         ]
-        
+
         if let userSession = SessionManager.shared?.activeUserSession {
             initialSyncObserver = ZMUserSession.addInitialSyncCompletionObserver(self, userSession: userSession)
         }
@@ -319,12 +318,12 @@ extension AuthenticationCoordinator: AuthenticationActioner, SessionManagerCreat
 
             case .updateBackendEnvironment(let url):
                 companyLoginController?.updateBackendEnvironment(with: url)
-                
+
             case .startCompanyLogin(let code):
                 startCompanyLoginFlowIfPossible(linkCode: code)
             case .startSSOFlow:
                 startAutomaticSSOFlow()
-                
+
             case .startLoginFlow(let request):
                 startLoginFlow(request: request)
 
@@ -385,7 +384,6 @@ extension AuthenticationCoordinator {
 
 }
 
-
 // MARK: - Actions
 
 extension AuthenticationCoordinator {
@@ -416,7 +414,7 @@ extension AuthenticationCoordinator {
                   let unauthenticatedAccount = sessionManager.accountManager.account(with: accountId) else {
                 fatal("No unauthenticated account to log out from")
             }
-            
+
             sessionManager.delete(account: unauthenticatedAccount)
         }
     }
@@ -514,7 +512,7 @@ extension AuthenticationCoordinator {
             }
         }
     }
-    
+
     /// Sends the registration activation code.
     private func sendActivationCode(_ credentials: UnverifiedCredentials, _ user: UnregisteredUser, isResend: Bool) {
         presenter?.isLoadingViewVisible = true
@@ -530,11 +528,11 @@ extension AuthenticationCoordinator {
     }
 
     // MARK: - passcode
-    
+
     private func startPasscodeSetup() {
         stateController.transition(to: .passcodeSetup)
     }
-    
+
     // MARK: - Linear Registration
 
     /// Sets the marketing consent value for the user to be registered.
@@ -562,7 +560,6 @@ extension AuthenticationCoordinator {
         unregisteredUser[keyPath: keyPath] = newValue
         eventResponderChain.handleEvent(ofType: .registrationStepSuccess)
     }
-
 
     /// Creates the user on the backend and advances the state.
     private func finishRegisteringUser() {
@@ -722,7 +719,6 @@ extension AuthenticationCoordinator {
         }
     }
 
-
     // MARK: - Company Login
 
     var canStartCompanyLogin: Bool {
@@ -743,7 +739,7 @@ extension AuthenticationCoordinator {
             companyLoginController?.displayCompanyLoginPrompt()
         }
     }
-    
+
     /// Automatically start the SSO flow if possible
     private func startAutomaticSSOFlow() {
         companyLoginController?.startAutomaticSSOFlow()
