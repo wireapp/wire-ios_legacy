4e234df1efaf41e249ff5fe135c8aa42e36cef16
KaterinaWire <katerina@wire.com>
2020-09-03 17:43:27 +0300

    Modify settings

--- Wire-iOS/Sources/AppDelegate.swift
+++ Wire-iOS/Sources/AppDelegate.swift
@@ -111,7 +111,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        NotificationCenter.default.post(name: SessionManager.registerPushTokenNotificationName, object: nil, userInfo: ["deviceToken" : deviceToken])
+        SessionManager.shared?.updateDeviceToken(deviceToken)
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
