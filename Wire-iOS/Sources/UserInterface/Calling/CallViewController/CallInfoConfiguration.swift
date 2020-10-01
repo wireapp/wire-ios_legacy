255633865483a5f2ac51536dbea271db78f73362
David Henner <david.henner@wire.com>
2020-10-01 18:31:32 +0200

    Revert "[Feature] Conference calling badge (#4589)"
    This reverts commit d56af7f2e787b2ea6cb654cd748267819c118a8a.

--- Wire-iOS/Sources/UserInterface/Calling/CallViewController/CallInfoConfiguration.swift
+++ Wire-iOS/Sources/UserInterface/Calling/CallViewController/CallInfoConfiguration.swift
@@ -131,6 +131,7 @@ fileprivate extension VoiceChannel {
 }
 
 struct CallInfoConfiguration: CallInfoViewControllerInput  {
+
     let permissions: CallPermissionsConfiguration
     let isConstantBitRate: Bool
     let title: String
@@ -149,7 +150,6 @@ struct CallInfoConfiguration: CallInfoViewControllerInput  {
     let mediaManager: AVSMediaManagerInterface
     let networkQuality: NetworkQuality
     let userEnabledCBR: Bool
-    let isConferenceCall: Bool
 
     private let voiceChannelSnapshot: VoiceChannelSnapshot
 
@@ -180,7 +180,6 @@ struct CallInfoConfiguration: CallInfoViewControllerInput  {
         videoPlaceholderState = voiceChannel.videoPlaceholderState ?? preferedVideoPlaceholderState
         disableIdleTimer = voiceChannel.disableIdleTimer
         networkQuality = voiceChannel.networkQuality
-        isConferenceCall = voiceChannel.isConferenceCall
     }
 
     // This property has to be computed in order to return the correct call duration
