9dfa2d518333cb547a276da65bbde877baf397ad
Marco Maddalena <marco.maddalena1982@gmail.com>
2020-10-09 15:44:12 +0200

    - Created Main.Storyboard - Created Navigator Class - Created RootViewController Class

---
+++ Wire-iOS/Sources/AppRootRouter.swift
@@ -0,0 +1,291 @@
+//
+// Wire
+// Copyright (C) 2020 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UIKit
+import WireSyncEngine
+import avs
+
+// MARK: - AppRootRouter
+public class AppRootRouter: NSObject {
+    
+    // MARK: - Private Property
+    private let navigator: NavigatorProtocol
+    private let rootViewController: RootViewController
+    private var appStateCalculator = AppStateCalculator()
+    private var authenticationCoordinator: AuthenticationCoordinator?
+    
+    // MARK: - Private Set Property
+    private(set) var sessionManager: SessionManager?
+    
+    // MARK: - Initialization
+    
+    init(viewController: RootViewController, navigator: NavigatorProtocol) {
+        self.rootViewController = viewController
+        self.navigator = navigator
+        super.init()
+        appStateCalculator.delegate = self
+    }
+    
+    // MARK: - Public implementation
+    
+    public func start(launchOptions: LaunchOptions) {
+        transition(to: .headless, completion: { })
+        createAnsStartSessionManager(launchOptions: launchOptions)
+    }
+    
+    // MARK: - Private implementation
+    
+    private func createAnsStartSessionManager(launchOptions: LaunchOptions) {
+        guard
+            let appVersion = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String,
+            let url = Bundle.main.url(forResource: "session_manager", withExtension: "json"),
+            let configuration = SessionManagerConfiguration.load(from: url),
+            let mediaManager = AVSMediaManager.sharedInstance()
+        else {
+            return
+        }
+        
+        configuration.blacklistDownloadInterval = Settings.shared.blacklistDownloadInterval
+        let jailbreakDetector = JailbreakDetector()
+        
+        SessionManager.clearPreviousBackups()
+        SessionManager.create(appVersion: appVersion,
+                              mediaManager: mediaManager,
+                              analytics: Analytics.shared,
+                              delegate: appStateCalculator,
+                              showContentDelegate: nil, //TO DO: We must set it
+                              application: UIApplication.shared,
+                              environment: BackendEnvironment.shared,
+                              configuration: configuration,
+                              detector: jailbreakDetector) { sessionManager in
+                self.sessionManager = sessionManager
+                /* TO DO: Add all this delegation
+                self.sessionManagerCreatedSessionObserverToken = sessionManager.addSessionManagerCreatedSessionObserver(self)
+                self.sessionManagerDestroyedSessionObserverToken = sessionManager.addSessionManagerDestroyedSessionObserver(self)
+                self.sessionManager?.foregroundNotificationResponder = self
+                self.sessionManager?.showContentDelegate = self
+                self.sessionManager?.switchingDelegate = self
+                self.sessionManager?.urlActionDelegate = self
+                */
+                sessionManager.updateCallNotificationStyleFromSettings()
+                sessionManager.useConstantBitRateAudio = SecurityFlags.forceConstantBitRateCalls.isEnabled
+                    ? true
+                    : Settings.shared[.callingConstantBitRate] ?? false
+                sessionManager.useConferenceCalling = Settings.shared[.conferenceCalling] ?? false
+                sessionManager.start(launchOptions: launchOptions)}
+        
+    }
+}
+
+// MARK: - AppStateCalculatorDelegate
+extension AppRootRouter: AppStateCalculatorDelegate {
+    func appStateCalculator(_: AppStateCalculator,
+                            didCalculate appState: AppState,
+                            completion: @escaping () -> Void) {
+        applicationWillTransition(to: appState)
+        transition(to: appState, completion: completion)
+    }
+    
+    private func transition(to appState: AppState, completion: @escaping () -> Void) {
+        //        showContentDelegate = nil
+        //        resetAuthenticationCoordinatorIfNeeded(for: appState)
+        
+        let completionBlock = { [weak self] in
+            self?.applicationDidTransition(to: appState)
+            completion()
+        }
+        
+        switch appState {
+        case .blacklisted:
+            showBlacklisted(completion: completionBlock)
+        case .jailbroken:
+            showJailbroken(completion: completionBlock)
+        case .migrating:
+            showLaunchScreen(isLoading: true, completion: completionBlock)
+        case .unauthenticated(error: let error):
+//            mainWindow.tintColor = .black
+            AccessoryTextField.appearance(whenContainedInInstancesOf: [AuthenticationStepController.self]).tintColor = UIColor.Team.activeButton
+            
+            showUnauthenticatedFlow(error: error, completion: completionBlock)
+            
+        case .authenticated(completedRegistration: let completedRegistration, databaseIsLocked: _):
+//            UIColor.setAccentOverride(.undefined)
+//            mainWindow.tintColor = UIColor.accent()
+//            executeAuthenticatedBlocks()
+//            showAuthenticated(isComingFromRegistration: completedRegistration,
+//                              completion: completionBlock)
+            
+            // TO DO: Show Authenticated Case
+            showTempController(completion: completionBlock)
+            
+        case .headless:
+            showLaunchScreen(completion: completionBlock)
+        case .loading(account: let toAccount, from: let fromAccount):
+            showSkeleton(fromAccount: fromAccount,
+                         toAccount: toAccount,
+                         completion: completionBlock)
+        }
+    }
+}
+
+// MARK: - Navigation Helper
+extension AppRootRouter {
+    private func showBlacklisted(completion: @escaping () -> Void) {
+        let blockerViewController = BlockerViewController(context: .blacklist)
+        rootViewController.set(childViewController: blockerViewController,
+                               completion: completion)
+    }
+    
+    private func showJailbroken(completion: @escaping () -> Void) {
+        let blockerViewController = BlockerViewController(context: .jailbroken)
+        rootViewController.set(childViewController: blockerViewController,
+                               completion: completion)
+    }
+    
+    private func showLaunchScreen(isLoading: Bool = false, completion: @escaping () -> Void) {
+        let launchViewController = LaunchImageViewController()
+        isLoading
+            ? launchViewController.showLoadingScreen()
+            : ()
+        rootViewController.set(childViewController: launchViewController,
+                               completion: completion)
+    }
+    
+    private func showUnauthenticatedFlow(error: NSError?, completion: @escaping () -> Void) {
+        // Only execute handle events if there is no current flow
+        guard
+            self.authenticationCoordinator == nil ||
+                error?.userSessionErrorCode == .addAccountRequested ||
+                error?.userSessionErrorCode == .accountDeleted,
+            let sessionManager = SessionManager.shared
+        else {
+            return
+        }
+        
+        let navigationController = SpinnerCapableNavigationController(navigationBarClass: AuthenticationNavigationBar.self,
+                                                                      toolbarClass: nil)
+        
+        authenticationCoordinator = AuthenticationCoordinator(presenter: navigationController,
+                                                              sessionManager: sessionManager,
+                                                              featureProvider: BuildSettingAuthenticationFeatureProvider(),
+                                                              statusProvider: AuthenticationStatusProvider())
+        
+        guard let authenticationCoordinator = authenticationCoordinator else {
+            return
+        }
+        
+        authenticationCoordinator.delegate = appStateCalculator
+        authenticationCoordinator.startAuthentication(with: error,
+                                                      numberOfAccounts: SessionManager.numberOfAccounts)
+        
+        rootViewController.set(childViewController: navigationController,
+                               completion: completion)
+    }
+    
+    private func showAuthenticated(isComingFromRegistration: Bool, completion: @escaping () -> Void) {
+        guard let selectedAccount = SessionManager.shared?.accountManager.selectedAccount else {
+            return
+        }
+        
+        let clientViewController = ZClientViewController(account: selectedAccount,
+                                                         selfUser: ZMUser.selfUser())
+        clientViewController.isComingFromRegistration = isComingFromRegistration
+        
+        /// show the dialog only when lastAppState is .unauthenticated and the user is not a team member, i.e. the user not in a team login to a new device
+        clientViewController.needToShowDataUsagePermissionDialog = false
+        
+        if case .unauthenticated(_) = self.appStateCalculator.previousAppState {
+            if SelfUser.current.isTeamMember {
+                TrackingManager.shared.disableCrashSharing = true
+                TrackingManager.shared.disableAnalyticsSharing = false
+            } else {
+                clientViewController.needToShowDataUsagePermissionDialog = true
+            }
+        }
+        
+        Analytics.shared.selfUser = SelfUser.current
+        
+        rootViewController.set(childViewController: clientViewController,
+                               completion: completion)
+    }
+    
+    private func showSkeleton(fromAccount: Account?, toAccount: Account, completion: @escaping () -> Void) {
+        let skeletonViewController = SkeletonViewController(from: fromAccount, to: toAccount)
+        rootViewController.set(childViewController: skeletonViewController,
+                               completion: completion)
+    }
+    
+    private func showTempController(completion: @escaping () -> Void) {
+        let viewController = UIViewController()
+        viewController.view.frame = UIScreen.main.bounds
+        viewController.view.backgroundColor = .red
+        rootViewController.set(childViewController: viewController,
+                               completion: completion)
+    }
+}
+
+// TO DO: THIS PART MUST BE CLENED UP
+extension AppRootRouter {
+    private func applicationWillTransition(to appState: AppState) {
+        /*
+        if case .authenticated = appState {
+            if AppDelegate.shared.shouldConfigureSelfUserProvider {
+                SelfUser.provider = ZMUserSession.shared()
+            }
+            callWindow.callController.transitionToLoggedInSession()
+        }
+
+        let colorScheme = ColorScheme.default
+        colorScheme.accentColor = .accent()
+        colorScheme.variant = Settings.shared.colorSchemeVariant
+        */
+    }
+    
+    private func applicationDidTransition(to appState: AppState) {
+        /*
+        if case .authenticated = appState {
+            callWindow.callController.presentCallCurrentlyInProgress()
+            ZClientViewController.shared?.legalHoldDisclosureController?.discloseCurrentState(cause: .appOpen)
+        } else if AppDelegate.shared.shouldConfigureSelfUserProvider {
+            SelfUser.provider = nil
+        }
+        
+        guard
+            case .unauthenticated(let error) = appState,
+            error?.userSessionErrorCode == .accountDeleted,
+            let reason = error?.userInfo[ZMAccountDeletedReasonKey] as? ZMAccountDeletedReason
+        else {
+            return
+        }
+        
+        presentAlertForDeletedAccount(reason)
+        */
+    }
+    
+    private func presentAlertForDeletedAccount(_ reason: ZMAccountDeletedReason) {
+        /*
+        switch reason {
+        case .sessionExpired:
+            presentAlertWithOKButton(title: "account_deleted_session_expired_alert.title".localized,
+                                     message: "account_deleted_session_expired_alert.message".localized)
+        default:
+            break
+        }
+        */
+    }
+}
---
+++ Wire-iOS/Sources/RootViewController.swift
@@ -0,0 +1,150 @@
+//
+// Wire
+// Copyright (C) 2020 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    private var childViewController: UIViewController?
+    
+    override var childForStatusBarStyle: UIViewController? {
+        return childViewController
+    }
+    
+    override var childForStatusBarHidden: UIViewController? {
+        return childViewController
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func set(childViewController newViewController: UIViewController?,
+             animated: Bool = false,
+             completion: (() -> Void)? = nil) {
+        if let newViewController = newViewController, let previousViewController = childViewController {
+            transition(
+                from: previousViewController,
+                to: newViewController,
+                animated: animated,
+                completion: completion)
+        } else if let newViewController = newViewController {
+            contain(newViewController, completion: completion)
+        } else {
+            removeChildViewController(animated: animated, completion: completion)
+        }
+        
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    private func contain(_ newViewController: UIViewController, completion: (() -> Void)?) {
+        add(newViewController)
+        newViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        newViewController.view.frame = view.bounds
+        view.addSubview(newViewController.view)
+        newViewController.didMove(toParent: self)
+        childViewController = newViewController
+        completion?()
+    }
+    
+    private func removeChildViewController(animated: Bool, completion: (() -> Void)?) {
+        let animationGroup = DispatchGroup()
+        if childViewController?.presentedViewController != nil {
+            animationGroup.enter()
+            childViewController?.dismiss(animated: animated) {
+                animationGroup.leave()
+            }
+        }
+        
+        childViewController?.willMove(toParent: nil)
+        childViewController?.view.removeFromSuperview()
+        childViewController?.removeFromParent()
+        childViewController = nil
+        
+        animationGroup.notify(queue: .main) {
+            completion?()
+        }
+    }
+    
+    private func transition(from fromViewController: UIViewController,
+                            to toViewController: UIViewController,
+                            animated: Bool = false,
+                            completion: (() -> Void)?) {
+        let animationGroup = DispatchGroup()
+        
+        if fromViewController.presentedViewController != nil {
+            animationGroup.enter()
+            fromViewController.dismiss(animated: animated) {
+                animationGroup.leave()
+            }
+        }
+        
+        fromViewController.willMove(toParent: nil)
+        addChild(toViewController)
+        
+        toViewController.view.frame = fromViewController.view.bounds
+        
+        animationGroup.enter()
+        transition(
+            from: fromViewController,
+            to: toViewController,
+            duration: 0.5,
+            options: .transitionCrossDissolve,
+            animations: {
+                self.view.bringSubviewToFront(fromViewController.view)
+                fromViewController.view.alpha = 0
+            }, completion: { _ in
+                fromViewController.removeFromParent()
+                toViewController.didMove(toParent: self)
+                animationGroup.leave()
+            })
+        
+        childViewController = toViewController
+        
+        animationGroup.notify(queue: .main) {
+            completion?()
+        }
+    }
+}
+
+extension UIViewController {
+    func add(_ child: UIViewController) {
+        self.add(child, to: view)
+    }
+    
+    func add(_ child: UIViewController, to view: UIView, pinToSuperview: Bool = true) {
+        child.willMove(toParent: self)
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+        if pinToSuperview {
+            child.view.translatesAutoresizingMaskIntoConstraints = false
+            child.view.pin(to: view)
+        }
+    }
+
+    func removeChild(_ viewController: UIViewController?) {
+        viewController?.willMove(toParent: nil)
+        viewController?.view.removeFromSuperview()
+        viewController?.removeFromParent()
+    }
+
+    func childViewController<T: UIViewController>(with type: T.Type) -> T? {
+        return children.compactMap { $0 as? T }.first
+            ?? children.compactMap { $0.childViewController(with: type).flatMap { $0 } }.first
+    }
+}
