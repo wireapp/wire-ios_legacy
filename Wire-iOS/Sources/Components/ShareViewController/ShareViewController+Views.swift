dfc98beb871e037ccbf2358f4e5a8737f70375de
bill chan <billypchan@gmail.com>
2020-10-12 16:38:25 +0200

    lint

--- Wire-iOS/Sources/Components/ShareViewController/ShareViewController+Views.swift
+++ Wire-iOS/Sources/Components/ShareViewController/ShareViewController+Views.swift
@@ -21,30 +21,30 @@ import UIKit
 import Cartography
 
 extension ShareViewController {
-    
+
     func createShareablePreview() {
         let shareablePreviewView = self.shareable.previewView()
         shareablePreviewView?.layer.cornerRadius = 4
         shareablePreviewView?.clipsToBounds = true
         self.shareablePreviewView = shareablePreviewView
-        
+
         let shareablePreviewWrapper = UIView()
         shareablePreviewWrapper.clipsToBounds = false
         shareablePreviewWrapper.layer.shadowOpacity = 1
         shareablePreviewWrapper.layer.shadowRadius = 8
         shareablePreviewWrapper.layer.shadowOffset = CGSize(width: 0, height: 8)
         shareablePreviewWrapper.layer.shadowColor = UIColor(white: 0, alpha: 0.4).cgColor
-        
+
         shareablePreviewWrapper.addSubview(shareablePreviewView!)
         self.shareablePreviewWrapper = shareablePreviewWrapper
-        
+
         self.shareablePreviewWrapper?.isHidden = !showPreview
     }
-    
+
     func createViews() {
-        
+
         createShareablePreview()
-        
+
         self.tokenField.textColor = .white
         self.tokenField.clipsToBounds = true
         self.tokenField.layer.cornerRadius = 4
@@ -60,7 +60,7 @@ extension ShareViewController {
         self.tokenField.textView.textContainerInset = UIEdgeInsets(top: 9, left: 40, bottom: 11, right: 12)
         self.tokenField.textView.backgroundColor = UIColor.from(scheme: .tokenFieldBackground, variant: .dark)
         self.tokenField.delegate = self
-        
+
         self.destinationsTableView.backgroundColor = .clear
         self.destinationsTableView.register(ShareDestinationCell<D>.self, forCellReuseIdentifier: ShareDestinationCell<D>.reuseIdentifier)
         self.destinationsTableView.separatorStyle = .none
@@ -69,11 +69,11 @@ extension ShareViewController {
         self.destinationsTableView.keyboardDismissMode = .interactive
         self.destinationsTableView.delegate = self
         self.destinationsTableView.dataSource = self
-        
+
         self.closeButton.accessibilityLabel = "close"
         self.closeButton.setIcon(.cross, size: .tiny, for: .normal)
         self.closeButton.addTarget(self, action: #selector(ShareViewController.onCloseButtonPressed(sender:)), for: .touchUpInside)
-        
+
         self.sendButton.accessibilityLabel = "send"
         self.sendButton.isEnabled = false
         self.sendButton.setIcon(.send, size: .tiny, for: .normal)
@@ -83,32 +83,31 @@ extension ShareViewController {
         self.sendButton.setBorderColor(.clear, for: .disabled)
         self.sendButton.circular = true
         self.sendButton.addTarget(self, action: #selector(ShareViewController.onSendButtonPressed(sender:)), for: .touchUpInside)
-        
+
         if self.allowsMultipleSelection {
             self.searchIcon.setIcon(.search, size: .tiny, color: .white)
-        }
-        else {
+        } else {
             self.searchIcon.isHidden = true
             self.sendButton.isHidden = true
             self.closeButton.isHidden = true
             self.bottomSeparatorLine.isHidden = true
         }
-        
+
         [self.blurView, self.containerView].forEach(self.view.addSubview)
         [self.tokenField, self.destinationsTableView, self.closeButton, self.sendButton, self.bottomSeparatorLine, self.topSeparatorView, self.searchIcon].forEach(self.containerView.addSubview)
-        
+
         if let shareablePreviewWrapper = self.shareablePreviewWrapper {
             self.containerView.addSubview(shareablePreviewWrapper)
         }
     }
-    
+
     func createConstraints() {
-        
+
         guard let shareablePreviewWrapper = shareablePreviewWrapper,
             let shareablePreviewView = shareablePreviewView else {
                 return
         }
-        
+
         [view,
          blurView,
          containerView,
@@ -122,7 +121,7 @@ extension ShareViewController {
          closeButton,
          sendButton
             ].disableAutoresizingMaskTranslation()
-        
+
         let shareablePreviewWrapperMargin: CGFloat = 16
         let tokenFieldMargin: CGFloat = 8
         let sendButtonMargin: CGFloat = 12
@@ -130,65 +129,65 @@ extension ShareViewController {
         let bottomConstraint = containerView.bottomAnchor.constraint(equalTo: view.safeBottomAnchor)
         let shareablePreviewTopConstraint = shareablePreviewWrapper.topAnchor.constraint(equalTo: containerView.safeTopAnchor, constant: shareablePreviewWrapperMargin)
         let tokenFieldShareablePreviewSpacingConstraint = tokenField.topAnchor.constraint(equalTo: shareablePreviewWrapper.bottomAnchor, constant: shareablePreviewWrapperMargin)
-        
+
         let tokenFieldTopConstraint = tokenField.topAnchor.constraint(equalTo: containerView.topAnchor, constant: tokenFieldMargin)
-        
+
         let tokenFieldHeightConstraint: NSLayoutConstraint
         let allowsMultipleSelectionConstraints: [NSLayoutConstraint]
         if allowsMultipleSelection {
             tokenFieldHeightConstraint = tokenField.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
-            
+
             allowsMultipleSelectionConstraints = [
                 closeButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
                 closeButton.centerYAnchor.constraint(equalTo: sendButton.centerYAnchor),
                 closeButton.widthAnchor.constraint(equalToConstant: 44),
                 closeButton.widthAnchor.constraint(equalTo: closeButton.heightAnchor),
-                
+
                 sendButton.topAnchor.constraint(equalTo: bottomSeparatorLine.bottomAnchor, constant: sendButtonMargin),
                 sendButton.widthAnchor.constraint(equalToConstant: 32),
                 sendButton.widthAnchor.constraint(equalTo: sendButton.heightAnchor),
                 sendButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-                sendButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: sendButtonMargin),
+                sendButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: sendButtonMargin)
 
             ]
         } else {
             tokenFieldHeightConstraint = tokenField.heightAnchor.constraint(equalToConstant: 0)
-            
+
             allowsMultipleSelectionConstraints = [
                 bottomSeparatorLine.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
             ]
         }
-        
+
         NSLayoutConstraint.activate([
             blurView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             blurView.topAnchor.constraint(equalTo: view.topAnchor),
             blurView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             blurView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
+
             containerView.topAnchor.constraint(equalTo: view.topAnchor),
             bottomConstraint,
             containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
+
             shareablePreviewTopConstraint,
             shareablePreviewWrapper.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: shareablePreviewWrapperMargin),
             shareablePreviewWrapper.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -shareablePreviewWrapperMargin),
-            
+
             shareablePreviewView.leadingAnchor.constraint(equalTo: shareablePreviewWrapper.leadingAnchor),
             shareablePreviewView.topAnchor.constraint(equalTo: shareablePreviewWrapper.topAnchor),
             shareablePreviewView.trailingAnchor.constraint(equalTo: shareablePreviewWrapper.trailingAnchor),
             shareablePreviewView.bottomAnchor.constraint(equalTo: shareablePreviewWrapper.bottomAnchor),
-            
+
             tokenFieldShareablePreviewSpacingConstraint,
             tokenFieldTopConstraint,
-            
+
             tokenField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: tokenFieldMargin),
             tokenField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -tokenFieldMargin),
             tokenFieldHeightConstraint,
-            
+
             searchIcon.centerYAnchor.constraint(equalTo: tokenField.centerYAnchor),
             searchIcon.leadingAnchor.constraint(equalTo: tokenField.leadingAnchor, constant: 8), // the search icon glyph has whitespaces,
-            
+
             topSeparatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             topSeparatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             topSeparatorView.topAnchor.constraint(equalTo: destinationsTableView.topAnchor),
@@ -201,18 +200,16 @@ extension ShareViewController {
 
             bottomSeparatorLine.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             bottomSeparatorLine.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            bottomSeparatorLine.heightAnchor.constraint(equalToConstant: .hairline),
-
+            bottomSeparatorLine.heightAnchor.constraint(equalToConstant: .hairline)
 
         ] + allowsMultipleSelectionConstraints)
-        
+
         self.bottomConstraint = bottomConstraint
         self.shareablePreviewTopConstraint = shareablePreviewTopConstraint
         self.tokenFieldShareablePreviewSpacingConstraint = tokenFieldShareablePreviewSpacingConstraint
         self.tokenFieldTopConstraint = tokenFieldTopConstraint
-        
-                
+
         updateShareablePreviewConstraint()
     }
-    
+
 }
