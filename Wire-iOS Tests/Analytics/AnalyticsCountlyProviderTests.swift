1c794085c8c1938c7fd3b312f6036c207f0eb855
bill chan <billypchan@gmail.com>
2020-10-29 18:31:37 +0100

    clean up

--- Wire-iOS Tests/Analytics/AnalyticsCountlyProviderTests.swift
+++ Wire-iOS Tests/Analytics/AnalyticsCountlyProviderTests.swift
@@ -67,9 +67,8 @@ final class AnalyticsCountlyProviderTests: XCTestCase, CoreDataFixtureTestHelper
 
             //GIVEN
             sut = Analytics(optedOut: false)
-            let analyticsCountlyProvider = AnalyticsCountlyProvider(countlyInstanceType: MockCountly.self ,
-                                                                    countlyAppKey: "dummy countlyAppKey"
-                )!
+            let analyticsCountlyProvider = AnalyticsCountlyProvider(countlyInstanceType: MockCountly.self,
+                                                                    countlyAppKey: "dummy countlyAppKey")!
             sut.provider = analyticsCountlyProvider
 
             //WHEN
