a6512744e693adcd6f166ab7c75c2ed16bc5b7b6
Bill Chan <billypchan@gmail.com>
2020-12-09 09:42:24 +0100

    Update BackgroundViewController.swift

--- Wire-iOS/Sources/UserInterface/ConversationList/BackgroundViewController.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/BackgroundViewController.swift
@@ -62,10 +62,6 @@ final class BackgroundViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    deinit {
-        userObserverToken = nil
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
