98a56c994a7101501d2c2bfcb7607ebe32778ce5
David Henner <david.henner@wire.com>
2021-02-01 13:02:45 +0100

    update capture state on didMoveToWindow

--- Wire-iOS/Sources/UserInterface/Calling/VideoGridView/SelfVideoPreviewView.swift
+++ Wire-iOS/Sources/UserInterface/Calling/VideoGridView/SelfVideoPreviewView.swift
@@ -61,7 +61,7 @@ final class SelfVideoPreviewView: BaseVideoPreviewView {
         super.didMoveToWindow()
         
         if window != nil {
-            startCapture()
+            updateCaptureState()
         }
     }
     
