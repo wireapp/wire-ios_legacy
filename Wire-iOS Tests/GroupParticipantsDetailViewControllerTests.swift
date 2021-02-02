3fb3ad0bb0e737403bbcbb2f34bafd1d3ce4d4e3
bill chan <billypchan@gmail.com>
2021-02-02 17:36:22 +0100

    lint

--- Wire-iOS Tests/GroupParticipantsDetailViewControllerTests.swift
+++ Wire-iOS Tests/GroupParticipantsDetailViewControllerTests.swift
@@ -20,17 +20,17 @@ import XCTest
 @testable import Wire
 import SnapshotTesting
 
-///TODO: retire this extension
+/// TODO: retire this extension
 extension ZMConversation {
-    
+
     func add(participants: Set<ZMUser>) {
         addParticipantsAndUpdateConversationState(users: participants, role: nil)
     }
-    
+
     func add(participants: [ZMUser]) {
         add(participants: Set(participants))
     }
-    
+
     func add(participants: ZMUser...) {
         add(participants: Set(participants))
     }
@@ -38,63 +38,60 @@ extension ZMConversation {
 
 private final class MockConversation: MockStableRandomParticipantsConversation, GroupDetailsConversation, SortedOtherParticipantsProvider {
     var sortedOtherParticipants: [UserType] = []
-    
+
     var userDefinedName: String?
-    
+
     var sortedServiceUsers: [UserType] = []
-    
+
     var hasReadReceiptsEnabled: Bool = false
-    
+
     var freeParticipantSlots: Int = 1
 }
 
 final class GroupParticipantsDetailViewControllerTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
-        
+
         SelfUser.setupMockSelfUser()
     }
-    
+
     override func tearDown() {
-        //        resetColorScheme()
-        
-        /// restore to default light scheme
-        //        ColorScheme.default.variant = .light
+        SelfUser.provider = nil
         
         super.tearDown()
     }
-    
+
     func testThatItRendersALotOfUsers() {
         // given
         let users: [MockUserType] = (0..<20).map {
             let user = MockUserType.createUser(name: "User #\($0)")
             return user
         }
-        
+
         let selected = Array(users.dropLast(15))
         let conversation = MockConversation()
         conversation.sortedOtherParticipants = users
-        
+
         // when & then
         verifyInAllColorSchemes(createSut: {
             let sut = GroupParticipantsDetailViewController(selectedParticipants: selected, conversation: conversation)
             return sut.wrapInNavigationController()
         })
-        
+
     }
-    
+
     func testEmptyState() {
         // given
         let conversation = MockConversation()
-        
+
         // when
         let sut = GroupParticipantsDetailViewController(selectedParticipants: [], conversation: conversation)
         sut.viewModel.admins = []
         sut.viewModel.members = []
         sut.setupViews()
         sut.participantsDidChange()
-        
+
         // then
         let wrapped = sut.wrapInNavigationController()
         verify(matching: wrapped)
