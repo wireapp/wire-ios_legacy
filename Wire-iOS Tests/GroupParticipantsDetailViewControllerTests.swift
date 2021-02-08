870cc40d625980bc99c15bb4ce7bbccf80df9de8
Bill Chan <billypchan@gmail.com>
2021-02-08 17:46:48 +0100

    Refactor MessageReplyPreviewViewTests with Swift snapshot testing (#4895)
    * testThatItRendersTextMessagePreview with both color scheme
    
    * testThatItRendersMention
    
    * fixed MessageReplyPreviewViewTests
    
    * clean up
    
    * lint

--- Wire-iOS Tests/GroupParticipantsDetailViewControllerTests.swift
+++ Wire-iOS Tests/GroupParticipantsDetailViewControllerTests.swift
@@ -55,7 +55,7 @@ final class GroupParticipantsDetailViewControllerTests: XCTestCase {
 
     override func tearDown() {
         SelfUser.provider = nil
-        
+
         super.tearDown()
     }
 
@@ -70,10 +70,12 @@ final class GroupParticipantsDetailViewControllerTests: XCTestCase {
         conversation.sortedOtherParticipants = users
 
         // when & then
-        verifyInAllColorSchemes(createSut: {
-            let sut = GroupParticipantsDetailViewController(selectedParticipants: selected, conversation: conversation)
-            return sut.wrapInNavigationController()
-        })
+		let createSut: () -> UIViewController = {
+			let sut = GroupParticipantsDetailViewController(selectedParticipants: selected, conversation: conversation)
+			return sut.wrapInNavigationController()
+		}
+
+        verifyInAllColorSchemes(createSut: createSut)
 
     }
 
