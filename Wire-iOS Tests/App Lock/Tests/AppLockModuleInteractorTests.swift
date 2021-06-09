b8e2e3335b388c42358bc4af4acb457911de0f95
Sven Jost <sven@wire.com>
2021-06-09 12:06:38 +0200

    Annotate tests for BSI in AppLockModuleInteractorTests.swift

--- Wire-iOS Tests/App Lock/Tests/AppLockModuleInteractorTests.swift
+++ Wire-iOS Tests/App Lock/Tests/AppLockModuleInteractorTests.swift
@@ -58,6 +58,7 @@ final class AppLockModuleInteractorTests: XCTestCase {
 
     // MARK: - Initiate authentication
 
+    // @SF.Locking @SF.Storage @TSFI.Client_Platform
     func test_InitiateAuthentication_NeedsToCreateCustomPasscode_Required() {
         // Given
         appLock.isCustomPasscodeSet = false
@@ -83,6 +84,7 @@ final class AppLockModuleInteractorTests: XCTestCase {
         XCTAssertEqual(presenter.results, [.customPasscodeCreationNeeded(shouldInform: false)])
     }
 
+    // @SF.Locking @SF.Storage @TSFI.Client_Platform
     func test_InitiaAuthentication_NeedsToCreatePasscode_InformingUserOfConfigChange() {
         // Given
         appLock.isCustomPasscodeSet = false
@@ -132,6 +134,7 @@ final class AppLockModuleInteractorTests: XCTestCase {
         XCTAssertEqual(presenter.results, [.readyForAuthentication(shouldInform: false)])
     }
 
+    // @SF.Locking @SF.Storage @TSFI.Client_Platform
     func test_InitiateAuthentication_SessionIsAlreadyUnlocked() {
         // Given
         session.lock = .none
@@ -144,6 +147,7 @@ final class AppLockModuleInteractorTests: XCTestCase {
         XCTAssertEqual(appLock.methodCalls.open.count, 1)
     }
 
+    // @SF.Locking @SF.Storage @TSFI.Client_Platform
     func test_InitiateAuthentication_RequireActiveApp_ReturnsNothingIfAppIsInBackground() {
         // Given
         applicationStateProvider.applicationState = .background
@@ -261,6 +265,7 @@ final class AppLockModuleInteractorTests: XCTestCase {
         XCTAssertEqual(presenter.results, [.authenticationDenied(.faceID)])
     }
 
+    // @SF.Locking @SF.Storage @TSFI.Client_Platform
     func test_EvaluateAuthentication_NeedCustomPasscode() {
         // Given
         session.lock = .screen
