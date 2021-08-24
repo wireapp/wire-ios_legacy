c0ab01002dbc7473d2ff46daa4b6e0d532d09249
KaterinaWire <katerina@wire.com>
2021-08-25 01:38:19 +0200

    Update alerts

--- Wire-iOS/Sources/UserInterface/MainController/ZClientViewController+ConferenceCallingAvailabilty.swift
+++ Wire-iOS/Sources/UserInterface/MainController/ZClientViewController+ConferenceCallingAvailabilty.swift
@@ -27,8 +27,43 @@ extension ZClientViewController {
     // Feature Service, which will then post other notifications.
 
     func presentConferenceCallingAvailableAlert() {
-        let title = "Wire enterprise"
-        let message = "Your team is was upgraded to Wire enterprise, which gives you access to features such as conference calls and more."
+        typealias ConferenceCallingAlert = L10n.Localizable.FeatureConfig.Update.ConferenceCalling.Alert
+        let title = ConferenceCallingAlert.title
+        let message = ConferenceCallingAlert.message
+        let learnMore = ConferenceCallingAlert.Message.learnMore
+
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alert.addAction(.ok())
+
+        let configuration = AlertConfiguration(message: message, subMessage: learnMore, link: URL.wr_wireEnterpriseLearnMore)
+        alert.setValue(createMessageView(configuration), forKey: "contentViewController")
+        present(alert, animated: true)
+    }
+
+    func presentConferenceCallingUnavailableAlertForAdmin() {
+        typealias ConferenceCallingRestrictions = L10n.Localizable.FeatureConfig.ConferenceCallingRestrictions.Admins
+        typealias ConferenceCallingAlert = ConferenceCallingRestrictions.Alert
+        let title = ConferenceCallingAlert.title
+        let message = ConferenceCallingAlert.message
+        let learnMore = ConferenceCallingAlert.Message.learnMore
+        let upgradeButtonTitle = ConferenceCallingRestrictions.UpgradeButton.title
+
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alert.addAction(.cancel())
+
+        alert.addAction(.init(title: upgradeButtonTitle, style: .default, handler: { (_) in
+            let browserViewController = BrowserViewController(url: URL.manageTeam(source: .settings))
+            self.present(browserViewController, animated: true)
+        }))
+        let configuration = AlertConfiguration(message: message, subMessage: learnMore, link: URL.wr_wirePricingLearnMore)
+        alert.setValue(createMessageView(configuration), forKey: "contentViewController")
+        present(alert, animated: true)
+    }
+
+    func presentConferenceCallingUnavailableAlertForMember() {
+        typealias ConferenceCallingAlert = L10n.Localizable.FeatureConfig.ConferenceCallingRestrictions.Members.Alert
+        let title = ConferenceCallingAlert.title
+        let message = ConferenceCallingAlert.message
 
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(.ok())
@@ -36,15 +71,41 @@ extension ZClientViewController {
         present(alert, animated: true)
     }
 
-    func presentConferenceCallingUnavailableAlert() {
-        let title = "Upgrade to enterprise"
-        let message = "Your team is currently on the free basic plan. Upgrade to Enterprise for access to features such as starting conferences and more."
 
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(.cancel())
-        alert.addAction(.init(title: "Upgrade now", style: .default, handler: nil))
+    private struct AlertConfiguration {
+        let message: String
+        let subMessage: String
+        let link: URL
+    }
 
-        present(alert, animated: true)
+    private func createMessageView(_ configuration: AlertConfiguration) -> UIViewController {
+        let controller = UIViewController()
+        let textView = UITextView()
+        textView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        textView.frame = controller.view.frame
+        textView.textContainerInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        textView.backgroundColor = .clear
+
+        let font = UIFont.mediumFont
+        let color = UIColor.from(scheme: .textForeground)
+
+        let linkAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.accent(),
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+            .underlineColor: UIColor.clear,
+            .font: font,
+            .link: configuration.link
+        ]
+
+        let message = (configuration.message && font && color && .lineSpacing(4)) + "\n" + (configuration.subMessage && linkAttributes)
+        textView.attributedText = message
+        textView.linkTextAttributes = [:]
+        textView.isEditable = false
+        textView.isUserInteractionEnabled = true
+
+        controller.view.addSubview(textView)
+
+        return controller
     }
 
 }
@@ -61,8 +122,9 @@ extension ZClientViewController: FeatureServiceDelegate {
         case .conferenceCallingIsAvailable:
             presentConferenceCallingAvailableAlert()
 
+            /// TODO katerina  ask what should we display in this case
         case .conferenceCallingIsUnavailable:
-            presentConferenceCallingUnavailableAlert()
+            presentConferenceCallingAvailableAlert()
         }
     }
 
@@ -77,7 +139,12 @@ extension ZClientViewController: ConferenceCallingUnavailableObserver {
     }
 
     func callCenterDidNotStartConferenceCall() {
-        presentConferenceCallingUnavailableAlert()
+        guard let selfUser = ZMUser.selfUser(),
+              selfUser.teamRole.isOne(of: .admin, .owner) else {
+            presentConferenceCallingUnavailableAlertForMember()
+            return
+        }
+        presentConferenceCallingUnavailableAlertForAdmin()
     }
 
 }
