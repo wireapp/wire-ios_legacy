c1c25af24608c8add46f2e28275eae3d677cfd46
Bill Chan <billypchan@gmail.com>
2021-10-27 15:58:38 +0200

    required public init?(coder aDecoder: NSCoder) {

--- Wire-iOS/Sources/UserInterface/Collections/TextSearch/TextSearchResultsView.swift
+++ Wire-iOS/Sources/UserInterface/Collections/TextSearch/TextSearchResultsView.swift
@@ -55,7 +55,8 @@ final public class TextSearchResultsView: UIView {
         }
     }
 
-    required public init?(coder aDecoder: NSCoder) {
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
