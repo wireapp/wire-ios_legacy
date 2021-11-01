27f72aa844d1323fe60f07fdb96ee15a964fe742
David Henner <david.henner@wire.com>
2021-11-01 15:45:41 +0100

    show federated username in user profile and conversations

--- Wire-iOS/Sources/UserInterface/ConnectRequests/UserConnectionView.swift
+++ Wire-iOS/Sources/UserInterface/ConnectRequests/UserConnectionView.swift
@@ -97,8 +97,9 @@ final class UserConnectionView: UIView, Copyable {
     }
 
     private var handleLabelText: NSAttributedString? {
-        guard let handle = user.handle, handle.count > 0 else { return nil }
-        return ("@" + handle) && [
+        guard let handle = user.handleDisplayString(withDomain: user.isFederated), !handle.isEmpty else { return nil }
+
+        return handle && [
             .foregroundColor: UIColor.from(scheme: .textDimmed),
             .font: FontSpec(.small, .semibold).font!
         ]
