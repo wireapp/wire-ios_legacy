07e6f84efd7a85fffd75f701573b161a96254848
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2022-01-07 11:04:53 +0100

    fix: SwiftLint warnings for legacy_constructor
    Legacy Constructor Violation: Swift constructors are preferred over legacy convenience functions. (legacy_constructor)

--- Wire-iOS Tests/BackupPasswordViewControllerTests.swift
+++ Wire-iOS Tests/BackupPasswordViewControllerTests.swift
@@ -39,8 +39,8 @@ class BackupPasswordViewControllerTests: ZMSnapshotTestCase {
             expectation.fulfill()
         }
         // WHEN
-        XCTAssertTrue(sut.textField(UITextField(), shouldChangeCharactersIn: NSMakeRange(0, 0), replacementString: validPassword))
-        XCTAssertFalse(sut.textField(UITextField(), shouldChangeCharactersIn: NSMakeRange(0, 0), replacementString: "\n"))
+        XCTAssertTrue(sut.textField(UITextField(), shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: validPassword))
+        XCTAssertFalse(sut.textField(UITextField(), shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: "\n"))
         // THEN
         self.waitForExpectations(timeout: 0.5) { error in
             XCTAssertNil(error)
@@ -53,7 +53,7 @@ class BackupPasswordViewControllerTests: ZMSnapshotTestCase {
             XCTFail("Sut is nil")
         }
         // WHEN
-        XCTAssertFalse(sut.textField(UITextField(), shouldChangeCharactersIn: NSMakeRange(0, 0), replacementString: "              "))
-        XCTAssertFalse(sut.textField(UITextField(), shouldChangeCharactersIn: NSMakeRange(0, 0), replacementString: "\n"))
+        XCTAssertFalse(sut.textField(UITextField(), shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: "              "))
+        XCTAssertFalse(sut.textField(UITextField(), shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: "\n"))
     }
 }
