97c438aca6079d22e9487708bd4ba2caacb58b62
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-05-31 10:27:03 +0200

    Snapshot test fix

--- Wire-iOS Tests/CountryTests.swift
+++ Wire-iOS Tests/CountryTests.swift
@@ -22,9 +22,9 @@ final class CountryTests: XCTestCase {
 
     func testThatCountryFromDeviceGivesCurrentCountry() {
         // GIVEN
-        let countryFromDevice = Country.countryFromDevice!
+        let countryFromDevice = Country.countryFromDevice
 
         // WHEN & THEN
-        XCTAssertEqual(countryFromDevice.iso, "us")
+        XCTAssertEqual(countryFromDevice?.iso, "us")
     }
 }
