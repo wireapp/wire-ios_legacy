643538c9803195ac1f3eb190f1c6b2663d8c81ed
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-06-17 14:17:20 +0200

    feat: apply new color design for switch - ACC-138

---
+++ WireCommonComponents/WireSwitch.swift
@@ -0,0 +1,37 @@
+//
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UIKit
+
+class WireSwitch: UISwitch {
+    init() {
+        super.init(frame: .zero)
+        
+        /*For on state*/
+        self.onTintColor = isEnabled ? SemanticColors.backgroundSwitchOnEnabled : SemanticColors.backgroundSwitchOnDisabled
+
+        /*For off state*/
+        self.layer.cornerRadius = self.frame.height / 2.0
+        self.backgroundColor = isEnabled ? SemanticColors.backgroundSwitchOffEnabled : SemanticColors.backgroundSwitchOffDisabled
+        self.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
