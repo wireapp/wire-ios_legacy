11932ac66bfc78c128ed478e715d9cc2aa01b36d
Paweł Brzozowski <pawelbrzozowskigplus@gmail.com>
2022-06-28 17:32:25 +0200

    Fix the contrast issue

--- WireCommonComponents/SemanticColors.swift
+++ WireCommonComponents/SemanticColors.swift
@@ -23,6 +23,7 @@ enum SemanticColors {
     static let headerLabelConversationDetails = UIColor(light: Asset.gray70, dark: Asset.gray50)
     static let textLabelTitleCellActive = UIColor(light: Asset.black, dark: Asset.white)
     static let textLabelSubtitleCellActive = UIColor(light: Asset.gray90, dark: Asset.white)
+    static let textLabelConversationDetailNoItems = UIColor(light: Asset.black, dark: Asset.gray20)
 }
 
 private extension UIColor {
