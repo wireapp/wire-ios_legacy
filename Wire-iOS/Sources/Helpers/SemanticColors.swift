cb314808da79858e0df1277dafca70d473353a60
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2022-07-06 12:24:02 +0200

    fix: SwiftLint warnings

--- Wire-iOS/Sources/Helpers/SemanticColors.swift
+++ Wire-iOS/Sources/Helpers/SemanticColors.swift
@@ -48,7 +48,7 @@ public enum SemanticColors {
 }
 
 extension UIColor {
-    
+
     convenience init(light: ColorAsset, dark: ColorAsset) {
         if #available(iOS 13.0, *) {
             self.init { traits in
@@ -63,11 +63,11 @@ extension UIColor {
             }
         }
     }
-    
+
 }
 
 public extension UIColor {
-    
+
     convenience init(for accentColor: AccentColor) {
         switch accentColor {
         case .blue:
@@ -86,10 +86,10 @@ public extension UIColor {
             self.init(light: Asset.purple500Light, dark: Asset.purple500Dark)
         }
     }
-    
+
     convenience init(fromZMAccentColor accentColor: ZMAccentColor) {
         let safeAccentColor = AccentColor(ZMAccentColor: accentColor) ?? .blue
         self.init(for: safeAccentColor)
     }
-    
+
 }
