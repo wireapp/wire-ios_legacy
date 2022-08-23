7f46a76883fd441dfef9f95f2513c6076a9abee0
Paweł Brzozowski <pawelbrzozowskigplus@gmail.com>
2022-08-23 11:50:50 +0200

    Update Wire-iOS/Sources/UserInterface/Availability/AvailabilityTitleView.swift
    Co-authored-by: David-Henner <david.henner@wire.com>

--- Wire-iOS/Sources/UserInterface/Availability/AvailabilityTitleView.swift
+++ Wire-iOS/Sources/UserInterface/Availability/AvailabilityTitleView.swift
@@ -118,7 +118,7 @@ final class AvailabilityTitleView: TitleView, Themeable, ZMUserObserver {
         if options.contains(.displayUserName) {
             title = user.name ?? ""
         } else if availability == .none && options.contains(.allowSettingStatus) {
-            title = "availability.message.set_status".localized
+            title = L10n.Localizable.Availability.Message.setStatus
         } else if availability != .none {
             title = availability.localizedName.localized
         }
