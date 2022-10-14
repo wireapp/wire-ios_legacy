3024c27b2b1d93e4e6f3e6ab0d73d74218d3ff8c
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2022-10-14 11:39:06 +0200

    fix: failing test

--- Wire-iOS/Sources/Authentication/Interface/Descriptions/ScreenDescriptions/Login/EmailLinkVerificationStepDescription.swift
+++ Wire-iOS/Sources/Authentication/Interface/Descriptions/ScreenDescriptions/Login/EmailLinkVerificationStepDescription.swift
@@ -62,8 +62,8 @@ final class EmailLinkVerificationMainView: NSObject, ViewDescriptor, ValueSubmis
         label.textColor = UIColor.Team.subtitleColor
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-
-        let button = SolidButtonDescription(title: "team.activation_code.button.resend".localized, accessibilityIdentifier: "resend_button")
+        
+        let button = SolidButtonDescription(title: L10n.Localizable.Team.ActivationCode.Button.resend.capitalized, accessibilityIdentifier: "resend_button")
         button.valueSubmitted = valueSubmitted
 
         let buttonView = button.create()
