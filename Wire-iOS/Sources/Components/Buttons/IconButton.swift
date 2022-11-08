a1cccc114c533757c3dafb363c23f39cf191499e
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2022-11-08 13:51:26 +0100

    Revert "chore: add explanation for the naming of the class"
    This reverts commit 3b6b917483cbb9aa4a1605db9835d46c081d9030.

--- Wire-iOS/Sources/Components/Buttons/IconButton.swift
+++ Wire-iOS/Sources/Components/Buttons/IconButton.swift
@@ -19,12 +19,6 @@
 import UIKit
 import WireCommonComponents
 
-// This is temporary until we finish with the accessibility project
-// This is named NonLegacyIconButton for the simple reason to keep
-// file changes to the minimum instead of renaming the oroginal IconButton
-// class to something else and then had to make changes in a lot more files
-
-// TODO: - [AGIS] Get rid of this class as soon as we make all the appropriate changes to the original class
 class NonLegacyIconButton: IconButton {
 
     override var isSelected: Bool {
