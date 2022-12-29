f0a7eab7908acc1b4c6ceca2a21fd22eac4a85fb
MarcinR <mratajczakmail@gmail.com>
2022-12-29 15:06:28 +0100

    code review

--- Wire-iOS/Sources/UserInterface/Calling/BottomSheetContainerViewController/CallingActionsInfoViewController/CallingActionsInfoViewController.swift
+++ Wire-iOS/Sources/UserInterface/Calling/BottomSheetContainerViewController/CallingActionsInfoViewController/CallingActionsInfoViewController.swift
@@ -60,7 +60,6 @@ class CallingActionsInfoViewController: UIViewController, UICollectionViewDelega
         }
     }
 
-
     var variant: ColorSchemeVariant = .light {
         didSet {
             updateAppearance()
@@ -112,6 +111,7 @@ class CallingActionsInfoViewController: UIViewController, UICollectionViewDelega
         participantsHeaderLabel.translatesAutoresizingMaskIntoConstraints = false
         participantsHeaderLabel.applyStyle(.headerLabel)
         participantsHeaderLabel.accessibilityTraits.insert(.header)
+        participantsHeaderLabel.text = L10n.Localizable.Call.Participants.showAll(participants.count).uppercased()
 
         let collectionView = CallParticipantsListView(collectionViewLayout: collectionViewLayout, selfUser: selfUser)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -121,7 +121,6 @@ class CallingActionsInfoViewController: UIViewController, UICollectionViewDelega
         [actionsView, participantsHeaderView, collectionView].forEach(stackView.addArrangedSubview)
         CallParticipantsListCellConfiguration.prepare(collectionView)
         view.backgroundColor = SemanticColors.View.backgroundDefaultWhite
-        participantsHeaderLabel.text = L10n.Localizable.Call.Participants.showAll(participants.count).uppercased()
     }
 
     private func createStackViewConstraints() {
@@ -129,10 +128,9 @@ class CallingActionsInfoViewController: UIViewController, UICollectionViewDelega
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             stackView.topAnchor.constraint(equalTo: view.topAnchor),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-
 
     private func createConstraints() {
         if !isIncomingCall {
--- Wire-iOS/Sources/UserInterface/Calling/BottomSheetContainerViewController/CallingBottomSheetViewController.swift
+++ Wire-iOS/Sources/UserInterface/Calling/BottomSheetContainerViewController/CallingBottomSheetViewController.swift
@@ -88,9 +88,9 @@ class CallingBottomSheetViewController: BottomSheetContainerViewController {
     // after rotating device recalculate bottom sheet max height
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        let isHorizontal = size.width > size.height
-        // if horizontal then bottom sheet should take whole screen (without headerBar)
-        let bottomSheetMaxHeight = isHorizontal ? (size.height - headerBar.bounds.height) : bottomSheetMaxHeight
+        let isLandscape = UIDevice.current.orientation.isLandscape
+        // if landscape then bottom sheet should take whole screen (without headerBar)
+        let bottomSheetMaxHeight = isLandscape ? (size.height - headerBar.bounds.height) : bottomSheetMaxHeight
         let newConfiguration = BottomSheetConfiguration(height: bottomSheetMaxHeight, initialOffset: bottomSheetMinimalOffset)
         guard self.configuration != newConfiguration else { return }
         self.configuration = newConfiguration
